9abce206e235314daa784e5c6ddfc4abcab48ad7
DB_046_UNFOUNDER <85393004+Anbu0646@users.noreply.github.com>
2022-09-25 17:04:00 +0530

    Update and rename Escape Speed.txt to Escape Speed.c

--- Escape Speed.c
+++ Escape Speed.c
@@ -1,3 +1,4 @@
+/*
 The program must accept three floating point values as G (gravitational constant), M (mass) and R (radius) of a planet. The program must calculate and print the escape speed of the object with precision up to 3 decimal places. 
 
 Formula: Escape speed = squareroot(2GM/R)
@@ -16,6 +17,7 @@ Output: 1.555
 SOLUTION:
 
 C:
+*/
 #include<stdio.h>
 #include<stdlib.h>
 int main()
