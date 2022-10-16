a5c94bd30deb23b4e4db8c49fa9a2c274c23b9b1
DB46_UNFOUNDERwithJ <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-10-16 12:39:31 +0530

    Create Largest Number in a string.c

---
+++ Largest Number in a string.c
@@ -0,0 +1,22 @@
+/*
+The program must accept a string S as the input. The program must print the largest number present in the string as the output. 
+
+Boundary Condition(s): 5 <= Length of String S <= 99 
+
+Input Format:  The first line contains the value of S. 
+Output Format: The first line contains the largest number in the string. 
+
+Example Input/Output 1: 
+
+Input:  hello23welcome76 
+Output: 76 
+
+Example Input/Output 2: 
+
+Input:  9good8morning4 
+Output: 9
+
+Solution:
+
+*/
+
