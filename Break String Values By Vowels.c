83c21e8627d355c09ed8070b35a2f79d3535fd2b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-11-07 19:55:55 +0530

    Create Break String Values By Vowels.c

---
+++ Break String Values By Vowels.c
@@ -0,0 +1,32 @@
+/*
+
+The program must accept a string S as the input. The program must break the string into multiple string values so that each string contains exactly one vowel. The string values must be broken such that all the string values start with vowel except for the first string. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format:  The first line contains the string S. 
+Output Format: The lines contain a string in each line. 
+
+Example Input/Output 1: 
+
+Input:  helloworld 
+Output: hell 
+        ow 
+        orld 
+        
+Explanation: The string values "hell", "ow" and "orld" contains exactly one vowel. 
+             All the string values are started with vowel except the first string. 
+             
+Example Input/Output 2: 
+
+Input:  daefifof 
+Output: da 
+        ef 
+        if 
+        of 
+        
+Explanation : The string values "da", "ef", "if" and "of" contains exactly one vowel. 
+              All the string values are started with vowel except the first string.
+              
+SOLUTION:
+*/
