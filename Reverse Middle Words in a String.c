9812c460b8474e3b6d75ec3e8cdc861b5c5fa4df
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-11-12 13:02:45 +0530

    Update and rename Reverse Middle Words in a String to Reverse Middle Words in a String.c

--- Reverse Middle Words in a String.c
+++ Reverse Middle Words in a String.c
@@ -21,4 +21,24 @@ Output: I ma fine
 SOLUTION:
 
 */
-
+// => @VikV
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char ch[1000][1000];
+    int n=0;
+    while(scanf("%s ", ch[n++])>0);
+    for(int i=0; i<n; i++)
+    {
+        int len=strlen(ch[i]);
+        for(int j=0; ch[i][j]!='\0'; j++)
+        {
+            if(i==0 || i==n-2)
+                printf("%c", ch[i][j]);
+            else
+                printf("%c", ch[i][len-j-1]);
+        }
+    printf(" ");
+    }
+}
