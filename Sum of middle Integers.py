22ecc89e7f0d6888bf786d8620ebfac75577033a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-12-10 13:05:43 +0530

    Create Sum of middle Integers.py

---
+++ Sum of middle Integers.py
@@ -0,0 +1,31 @@
+'''
+
+The program must accept N integers as the input. If N is even, the program must print the sum of middle four integers among N integers as the output. Else the program must print the sum of middle three integers among N integers as the output. 
+
+Boundary Condition(s): 5 <= N <= 100 1 <= Each Integer Value <= 9999 
+
+Input Format:  The first line contains the integer N. 
+               The second line contains N integers separated by space(s). 
+               
+Output Format: The first line contains either the sum of middle four integers or the sum of middle three integers among N integers. 
+
+Example Input/Output 1: 
+
+Input:  5 
+        2 3 5 7 6 
+Output: 15 
+
+Explanation: Here N = 5 which is odd. 
+             So the middle 3 integers are 3, 5 and 7. 
+             The sum of the middle three integers is 15 (3 + 5 + 7). 
+             Hence the output is 15. 
+             
+Example Input/Output 2: 
+
+Input:  6 
+        23 47 63 52 77 56 
+Output: 239
+
+SOLUTION:
+
+'''
