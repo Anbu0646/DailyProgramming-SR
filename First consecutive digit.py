4c85e4f2c0ee1ec3ce0c2d597b634b15afe397a8
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-12-18 22:05:40 +0530

    Create First consecutive digit.py

---
+++ First consecutive digit.py
@@ -0,0 +1,22 @@
+'''
+the program must accept an integer n as the input. the program must print the first occurring consecutive odd digits if the first digit is odd as the output. else the program must print the first occurring consecutive even digits as the output. 
+
+boundary condition(s): 1 <= n <= 10^8 
+
+input format:  the first line contains the integer n. 
+output format: the first line contains the consecutive digits without any space. 
+
+example input/output 1: 
+
+input:  157894 
+output: 157 
+
+explanation: the first digit is odd so the first occurring consecutive odd digits 1, 5 and 7 are printed. 
+
+example input/output 2: 
+
+input:  601648 
+output: 60
+
+SOLUTION:
+'''
