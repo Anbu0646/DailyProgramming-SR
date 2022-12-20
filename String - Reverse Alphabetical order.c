0bbf1b39c4b0e108d1b86d0bab2b4ac86ba68f75
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-12-20 19:30:55 +0530

    Update String - Reverse Alphabetical order.c

--- String - Reverse Alphabetical order.c
+++ String - Reverse Alphabetical order.c
@@ -1,8 +1,25 @@
 /*
+The program must accept a string S as the input. The program must print the alphabets of S in reversal alphabetical order as the output. Note: The string S always contain only lowercase alphabets.
 
+Boundary Condition(s): 1 <= Length of S <= 100
 
+Input Format:  The first line contains the string S.
+Output Format: The first line contains the alphabets of S in reversal alphabetical order.
 
+Example Input/Output 1:
 
+Input:  orange
+Output: rongea
+
+Explanation: In the string orange, the alphabets in reversal alphabetical order are r o n g e a.
+             Hence the output is rongea.
+             
+Example Input/Output 2:
+
+Input: sweet
+Ouput: wtsee
+
+SOLUTION:
 */
 
 // ==> Code by @JustNothing_Vishal
