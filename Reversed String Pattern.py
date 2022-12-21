788f2e6d3cd0fc04d0016c4d6e7c486da2b3793a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-12-21 09:02:07 +0530

    Create Reversed String Pattern.py

---
+++ Reversed String Pattern.py
@@ -0,0 +1,24 @@
+'''
+The program must accept a string value S as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= S <= 100 
+
+Input Format:  The first line contains the string S. 
+Output Format: The first N lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  happy 
+Output: h 
+        p a 
+        * y p 
+        
+Example Input/Output 2: 
+
+Input:  orange 
+Output: o 
+        a r 
+        e g n
+        
+SOLUTION:
+'''
