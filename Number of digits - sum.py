5ecb223dce9455822ff5744036ee3ee2c7e6d1d5
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2022-12-29 11:46:20 +0530

    Create Number of digits - sum.py

---
+++ Number of digits - sum.py
@@ -0,0 +1,28 @@
+'''
+The program must accept two integers X and Y as the input. The program must print their sum S if the number of digits in S is equal to X. Else the program must print the integer X as the output. 
+
+Boundary Condition(s): 1 <= X, Y <= 10^8 
+
+Input Format:  The first line contains the values of X and Y separated by a space. 
+Output Format: The first line contains either S or X. 
+
+Example Input/Output 1: 
+
+Input:  4 997 
+Output: 1001 
+
+Explanation: The sum of 4 and 997 is 1001. 
+             The number of digits in 1001 is 4 and it is equal to X. 
+             So print the sum 1001. 
+             Hence the output is 1001 
+             
+Example Input/Output 2: 
+
+Input:  8 3 
+Output: 8
+
+SOLUTION:
+'''
+#WednesdaywithJ&A
+x, y =  map(int, input().split())
+print(x+y if len(str(x+y))==x else x)
