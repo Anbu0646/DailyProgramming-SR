b605784959fad5a8b8ffffa6af2d8d7f6e8f136e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-01-08 12:05:20 +0530

    Create Equal First Half and Second Half String.c

---
+++ Equal First Half and Second Half String.c
@@ -0,0 +1,67 @@
+/*
+The program must accept a string S as the input. The program must print yes if the first half of the string is equal to the second half of the string. 
+Else the program must print no as the output. 
+
+Note: If the length of the string is odd ignore the character in the middle. 
+
+Boundary Condition(s): 1 <= Length of the string <= 1000 
+
+Input Format:  The first line contains the string S. 
+Output Format: The first line contains either yes or no. 
+
+Example Input/Output 1: 
+
+Input:  dumdum 
+Output: yes 
+
+Explanation: Both the first and second half of the string is same so yes is printed. 
+
+Example Input/Output 2: 
+
+Input:  YelYe 
+Output: yes 
+
+Example Input/Output 3: 
+
+Input:  tellet 
+Output: no
+
+
+SOLUTION:
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[1001];
+    scanf("%s", s);
+    int len=strlen(s);
+    if(len%2==1)
+    {
+        for(int i=0, j=len/2+1; i<len/2, j<len; i++, j++)
+        {
+            if(s[i]!=s[j])
+            {
+                printf("no");
+                return 0;
+            }
+        }
+        printf("yes");
+    }
+    else
+    {
+        for(int i=0, j=len/2;i <len/2, j<len; i++, j++)
+        {
+            if(s[i]!=s[j])
+            {
+                printf("no");
+                return 0;
+            }
+        }
+        printf("yes");
+    }
+    
+    
+
+}
