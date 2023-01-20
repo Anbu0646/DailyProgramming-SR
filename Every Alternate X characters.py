ac5b44d3bea5afe866a6b6244992a135e447989b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-01-20 22:18:24 +0530

    Create Every Alternate X characters.py

---
+++ Every Alternate X characters.py
@@ -0,0 +1,26 @@
+'''
+The program must accept a string S and an integer N as the input. The program must print every alternate N characters in S as the output. 
+
+Boundary Condition(s):  1 <= Length of String S <= 1000 
+                        1 <= N <= Length of String S 
+
+Input Format:  The first line contains S and N separated by space. 
+Output Format: The first line contains the string as per the given condition. 
+
+Example Input/Output 1: 
+
+Input:  replacement 3 
+Output: repeme 
+
+Explanation:  The N value is 3 so first 3 characters rep are printed. 
+              Then the next three characters lac are not printed. 
+              Then the next three characters eme are printed. 
+
+Example Input/Output 2: 
+
+Input:  framework 2 
+Output: frewk
+
+SOLUTION:
+'''
+
