68a50d508876c527687a80607ab428b39b546f7a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-14 20:17:58 +0530

    Create Zero sum columns.py

---
+++ Zero sum columns.py
@@ -0,0 +1,44 @@
+'''
+The program must accept an NxN matrix as the input. The program must print yes if the sum of each column in the matrix is equal to 0. 
+Else the program must print no as the output. 
+
+Boundary Condition(s): 1 <= N <= 100 
+
+Input Format:  The first line contains N. 
+               The next N lines contain N integers each separated by space. 
+Output Format: The first line contains yes or no.
+
+Example Input/Output 1:
+
+Input:   3 
+         10 -1 7 
+        -1 -8 1
+        -9 9 -8 
+Output: yes 
+
+Explanation: The sum of elements in the first column is 10 + (-1) + (-9) = 0. 
+             The sum of elements in the second column is (-1) + (-8) + 9 = 0. 
+             The sum of elements in the third column is 7 + 1 + (-8) = 0.
+
+Example Input/Output 2: 
+
+Input:   5
+         1 0 -3 -7 3 
+         5 2 -7 -6 -4 
+         8 -10 -6 -3 9
+        -10 -5 -9 3 -10 
+        -4 12 25 13 2
+Output: no
+
+SOLUTION:
+'''
+
+#Michael:
+n=int(input())
+m=[list(map(int, input().split())) for i in range(n)]
+f=0
+for j in range(n):
+    cs=sum(k[j] for k in m)
+    if(cs!=0):
+        f=1
+print("yes" if f==0 else "no")
