184bd238a0de1bef4bac13ad7d978edd78faf3a4
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-18 22:23:07 +0530

    Update Sum - Multiples of x.py

--- Sum - Multiples of x.py
+++ Sum - Multiples of x.py
@@ -33,9 +33,7 @@ n, s = map(int, input().split())
 l=list(map(int, input().split()))
 
 for i in range(1, n+1):
-    for j in range(1, n+1): 
-        if j%i==0:
-            c=sum(l[j - 1])
+    c=sum(l[j - 1] for j in range(1, n+1) if j % i == 0)
     if(c==s):
         print(i)
         break
