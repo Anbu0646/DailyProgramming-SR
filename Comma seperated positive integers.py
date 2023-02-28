ae9f064433d5501a4e373dc6e5de955e27db113b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-02-28 07:11:40 +0530

    Create Comma seperated positive integers.py

---
+++ Comma seperated positive integers.py
@@ -0,0 +1,26 @@
+'''
+The program must accept N integers separated by a comma as the input.
+The program must print the positive integers separated by a comma among the N integers as the output. 
+If there is no positive integer then the program must print -1 as the output.
+
+Boundary Condition(s):  1 <= N <= 100 
+-999 <= Each integer value <= 999 
+
+Input Format:  The first line contains the value of N. 
+The second line contains N integers separated by a comma.
+Output Format: The first line contains either the positive integers separated by a comma or -1. 
+
+Example Input/Output 1: 
+
+Input:  5 12,-10,78,-5,-9
+Output: 12,78 
+
+Explanation: The positive integers are 12 and 78. So they are separated by a comma and printed as the output.
+
+Example Input/Output 2: 
+
+Input:  6 -41,-93,-91,-54,-59,-88 
+Output: -1
+
+SOLUTION:
+'''
