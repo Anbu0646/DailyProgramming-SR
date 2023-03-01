ccdd66de075070d888fbd80fddac090a05d87d3c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-01 22:12:45 +0530

    Create Up-Down Arrow Pattern.py

---
+++ Up-Down Arrow Pattern.py
@@ -0,0 +1,47 @@
+'''
+The program must accept an integer N and a character CH as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s):  2 <= N <= 100 
+
+Input Format:  The first line contains the values of N and CH separated by a space. 
+Output Format: The first 3xN lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  4 * 
+
+Output: ---* 
+        --*** 
+        -*-*-* 
+        *--*--* 
+        ---* 
+        ---* 
+        ---* 
+        ---*
+        *--*--*
+        -*-*-* 
+        --***
+        ---* 
+        
+Example Input/Output 2:
+
+Input:  5 # 
+
+Output: ----#
+        ---### 
+        --#-#-#
+        -#--#--#
+        #---#---# 
+        ----# 
+        ----#
+        ----#
+        ----# 
+        ----#
+        #---#---#
+        -#--#--#
+        --#-#-#
+        ---### 
+        ----#
+
+SOLUTION:
+'''
