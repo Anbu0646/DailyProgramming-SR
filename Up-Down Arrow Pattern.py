92b49cc0f35fe76d889d5c22e7a6da53866fc8a4
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-02 20:03:00 +0530

    Update Up-Down Arrow Pattern.py

--- Up-Down Arrow Pattern.py
+++ Up-Down Arrow Pattern.py
@@ -45,3 +45,20 @@ Output: ----#
 
 SOLUTION:
 '''
+
+a, b = input().split()
+a = int(a)
+l = []
+p = 0
+l.append(("-" * (a-1)) + b)
+
+for i in range(a-2, -1, -1):
+    l.append( ("-"*i) + b + ("-"*p) + b + ("-"*p) + b)
+    p+=1
+
+print(*l, sep="\n")
+l = l[::-1]
+for i in range(a):
+    print( ("-"*(a-1) ) + b)
+
+print(*l, sep="\n")
