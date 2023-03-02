d80b9268fa91543e8fe5ad9391b8343ce3513510
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-02 20:00:12 +0530

    Create Replace the additional spaces.py

---
+++ Replace the additional spaces.py
@@ -0,0 +1,25 @@
+'''
+A string S with space is passed as the input to the program. 
+The program must print the words in S separated by exactly one space character and the additional space characters (following the first space character) must be replaced
+by asterisk (*) if present.
+
+Boundary Condition(s): 1 <= Length of S <= 1000 
+
+Input Format:  The first line contains the string S. 
+Output Format: The first line contains the words as per the given condition. 
+
+Example Input/Output 1:
+
+Input:  Practice makes perfect 
+Output: Practice *******makes ******perfect 
+
+Explanation: The additional spaces between words are replaced by asterisks and printed.
+
+Example Input/Output 2: 
+
+Input:   The process needed 30 minutes to finish 
+Output:  The **process ****needed ****30 *minutes ***to finish
+
+SOLUTION:
+'''
+
