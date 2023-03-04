373833af924c9f11e56a5bed5783a89181c801d8
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-04 12:10:02 +0530

    Create Dice game.py

---
+++ Dice game.py
@@ -0,0 +1,39 @@
+'''
+Pairs of integers are passed as the input to the program. The integers in each pair represent values of two dice in a game.
+If both the values are same, then the dice are rolled again. If the values are different, then the game ends. 
+The program must print the sum of total number of values obtained from the dice at the end of the game. 
+
+Boundary Condition(s): 1 <= Value of each integer <= 6 
+
+Input Format:  The lines contain two integers each separated by a space. 
+Output Format: The first line contains the sum of all values obtained from the dice. 
+
+Example Input/Output 1:
+
+Input:  4 4
+        3 3
+        2 5 
+Output: 21 
+
+Explanation: The dice are rolled until two different values are obtained. 
+So the total sum is 4 + 4 + 3 + 3 + 2 + 5 = 21 
+
+Example Input/Output 2: 
+
+Input:  5 5 
+        1 1 
+        2 2 
+        4 4 
+        5 4 
+Output: 33
+
+SOLUTION:
+'''
+
+s=0
+for i in range(1000):
+    d1, d2 = map(int, input().split())
+    s+=d1+d2
+    if(d1!=d2):
+        print(s)
+        break
