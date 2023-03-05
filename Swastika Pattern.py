01b5ee048c4fa136c336a0ca805e9aa373f5652b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-05 22:03:55 +0530

    Create Swastika Pattern.py

---
+++ Swastika Pattern.py
@@ -0,0 +1,30 @@
+'''
+The program must accept a positive odd integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 999
+
+Input Format:  The first line contains the value of N.
+Output Format: The list of lines containing the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1: 
+
+Input:  5 
+Output: 1 * 3 2 1 
+        2 * 4 * *
+        3 4 5 4 3 
+        * * 4 * 2
+        1 2 3 * 1 
+        
+Example Input/Output 2: 
+
+Input:  7 
+Output: 1 * * 4 3 2 1
+        2 * * 5 * * *
+        3 * * 6 * * *
+        4 5 6 7 6 5 4
+        * * * 6 * * 3
+        * * * 5 * * 2 
+        1 2 3 4 * * 1
+
+SOLUTION:
+'''
