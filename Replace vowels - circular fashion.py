0ec322bafa21623aea4f8d0111683d8be9a7fb6b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-06 09:50:28 +0530

    Create Replace vowels - circular fashion.py

---
+++ Replace vowels - circular fashion.py
@@ -0,0 +1,25 @@
+'''
+The program must accept a string S as the input. The program must replace all the vowels in S by the vowels a, e, i, o and u in a circular manner. 
+Finally, the program must print the modified string as the output. 
+
+Note: All the alphabets in S are only in lower case. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format:  The first line contains the string value S. 
+Output Format: The first line contains the modified string value of S. 
+
+Example Input/Output 1:
+
+Input:  kingkong 
+Output: kangkeng 
+
+Explanation: The vowels in the string kingkong are i and o. So they are replaced by a and e. Hence the output is kangkeng 
+
+Example Input/Output 2: 
+
+Input:  icecreamchocolate 
+Output: acecriomchucaleti
+
+SOLUTION:
+'''
