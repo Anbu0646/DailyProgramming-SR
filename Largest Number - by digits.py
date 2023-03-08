d2521aeefeac15f85b7927e8ae6e533fc49f1e98
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-08 22:12:39 +0530

    Create Largest Number - by digits.py

---
+++ Largest Number - by digits.py
@@ -0,0 +1,30 @@
+'''
+The program must accept N integers as the input. The program must print the largest integer for each set of integers having the same number of digits as the output. 
+The largest integers must be printed in the order starting from the integers having the least number of digits to integers having the most number of digits.
+
+Boundary Condition(s):  1 <= N <= 1000 
+                        1 <= Each integer value <= 10^9
+
+Input Format:  The first line contains N. The second line contain N integers separated by space.
+Output Format: The first line contains largest integer as given in the condition above. 
+
+Example Input/Output 1: 
+
+Input:  7 
+        32 458 98 12 805 8 2 
+
+Output: 8 98 805 
+
+Explanation:  The largest one digit integer among 8 and 2 is 8. 
+              The largest two digit integer among 32 98 and 12 is 98. 
+              The largest three digit integer among 458 and 805 is 805. 
+              
+Example Input/Output 2:
+
+Input:  8 
+        750 52 9066 330 275447 47 59 225 
+
+Output: 59 750 9066 275447
+
+SOLUTION:
+'''
