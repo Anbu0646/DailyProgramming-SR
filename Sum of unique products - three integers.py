136ddc68c7d396a3cc1b17600376c387acc1ea46
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-15 22:15:36 +0530

    Create Sum of unique products - three integers.py

---
+++ Sum of unique products - three integers.py
@@ -0,0 +1,24 @@
+'''
+The program must accept three integers as the input. The program must calculate the product of all the possible combinations of two integers among the three integers. 
+Then the program must print the sum of unique product values. 
+
+Boundary Condition(s): 1 <= Three integer values <= 10^6
+
+Input Format:  The first line contains the three integers separated by a space. 
+Output Format: The first line contains the sum as per the given conditions.
+
+Example Input/Output 1:
+
+Input:  10 5 7
+Output: 155 
+
+Explanation: The product of all the possible combinations of two integers are 50, 35 and 70. So their sum is 155.
+
+Example Input/Output 2: 
+
+Input:  5 10 5 
+Output: 75
+
+
+SOLUTION:
+'''
