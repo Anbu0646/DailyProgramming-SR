7d315183af444686490b2d91b14132ae68c4f1ea
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-15 19:50:37 +0530

    Create Dynamic V pattern.py

---
+++ Dynamic V pattern.py
@@ -0,0 +1,47 @@
+'''
+The program must accept two integers (X, Y) and a character CH as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= X, Y <= 1000 
+
+Input Format:  The first line contains the values of X, CH and Y separated by a space. 
+Output Format: The list of lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  3 # 5 
+
+Output: ------#
+        -----#
+        #---#
+        -#-# 
+        --# 
+
+Example Input/Output 2:
+
+Input:  7 @ 7 
+
+Output: @-----------@ 
+        -@---------@ 
+        --@-------@ 
+        ---@-----@
+        ----@---@ 
+        -----@-@
+        ------@
+
+Example Input/Output 3: 
+
+Input:  10 * 4 
+
+Output: *
+        -*
+        --* 
+        ---* 
+        ----* 
+        -----* 
+        ------*-----*
+        -------*---*
+        --------*-* 
+        ---------*
+
+SOLUTION:
+'''
