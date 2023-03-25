ff11e62212cc7484a8e4c67e9486dacc1727b9cd
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-25 13:39:42 +0530

    Create Double upto P - Pattern printing.py

---
+++ Double upto P - Pattern printing.py
@@ -0,0 +1,31 @@
+'''
+The program must accept two integers N and P where P <= N as input. 
+The program must print the pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s): 1 <= N <= 10 
+                       1 <= P <= N 
+                                            
+Input Format:  The first line contains the value of N and P separated by a space. 
+Output Format: The first N lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  5 3 
+Output: 1 1 
+        2 2 2 2 
+        3 3 3 3 3 3
+        4 4 4 4
+        5 5 5 5 5 
+        
+Example Input/Output 2: 
+
+Input:  6 2
+Output: 1 1
+        2 2 2 2
+        3 3 3 
+        4 4 4 4 
+        5 5 5 5 5 
+        6 6 6 6 6 6
+
+SOLUTION:
+'''
