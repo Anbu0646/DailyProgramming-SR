56de517f806e66502a953a227d1bdb1875ce1ee5
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-25 22:04:49 +0530

    Create Diamond - pattern printing.py

---
+++ Diamond - pattern printing.py
@@ -0,0 +1,28 @@
+'''
+The program must accept an odd integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 99 
+
+Input Format:  The first line contains the value of N.
+Output Format: The list of lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  5 
+
+Output: -********* 
+        ***-----*** 
+        --**---** 
+        ----*-* 
+        
+Example Input/Output 2: 
+
+Input:  7 
+Output: -************* 
+        ****-------**** 
+        --***-----*** 
+        ----**---** 
+        ------*-*
+
+SOLTUION:
+'''
