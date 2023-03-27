cae771a75893dc9fe783e1d1e2ab2598db6ae9bf
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-27 22:02:37 +0530

    Create String - Triangle Border Pattern.py

---
+++ String - Triangle Border Pattern.py
@@ -0,0 +1,28 @@
+'''
+The program must accept a string S as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Note: Length of S is always a multiple of 3. 
+
+Boundary Condition(s): 3 <= Length of S <= 99 
+
+Input Format:  The first line contains the string S. 
+Output Format: The list of lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  abcdef
+Output: --a 
+-f-b
+e-d-c 
+
+Example Input/Output 2: 
+
+Input:  skillrack 
+Output: ---s 
+--k-k 
+-c---i 
+a-r-l-l
+
+SOLUTION:
+'''
+
