2a0c39cab5c43386f57158ad7b0780623f72d7d0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-28 19:26:40 +0530

    Create Two Primes Sum.py

---
+++ Two Primes Sum.py
@@ -0,0 +1,21 @@
+'''
+Given an integer N as input, the program must print two prime numbers whose sum will be equal to given integer. 
+If there are several combinations possible, print only first such pair. Otherwise print -1 as output. 
+
+Boundary Condition(s): 2 <= N <= 999999 
+
+Input Format:  The first line contains the value of N. 
+Output Format: The first line contains two prime numbers whose sum is equal to given integer N otherwise -1. 
+
+Example Input/Output 1: 
+
+Input:  34 
+Output: 3 31 
+
+Example Input/Output 2: 
+
+Input:  1024 
+Output: 3 1021
+
+SOLUTION:
+'''
