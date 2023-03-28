e01e176c27725b5f1c74b1652e7c9b23211e82a4
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-03-28 22:06:31 +0530

    Create Spider Pattern Printing.py

---
+++ Spider Pattern Printing.py
@@ -0,0 +1,54 @@
+'''
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 100 
+
+Input Format:  The first line contains the value of N. 
+Output Format: The list of lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  4 
+
+Output: 
+        |||* * * * 
+        ||* * - * * 
+        |* - * * - * 
+        * * * * * * * 
+        |* - * * - * 
+        ||* * - * * 
+        |||* * * * 
+
+Example Input/Output 2: 
+
+Input:  5 
+
+Output: 
+        ||||* * * * * 
+        |||* * --- * * 
+        ||* - * - * - * 
+        |* --- * * --- * 
+        * * * * * * * * * 
+        |* --- * * --- * 
+        ||* - * - * - * 
+        |||* * --- * * 
+        ||||* * * * * 
+
+Example Input/Output 3: 
+
+Input:  6 
+Output: 
+        |||||* * * * * * 
+        ||||* * ----- * * 
+        |||* - * --- * - * 
+        ||* --- * - * --- * 
+        |* ----- * * ----- * 
+        * * * * * * * * * * * 
+        |* ----- * * ----- * 
+        ||* --- * - * --- * 
+        |||* - * --- * - * 
+        ||||* * ----- * * 
+        |||||* * * * * *
+
+SOLUTION:
+'''
