304cddf1408aa9af488f940635ea8aecc801923f
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-02 22:10:39 +0530

    Create First Non-Repeating Character - String.py

---
+++ First Non-Repeating Character - String.py
@@ -0,0 +1,24 @@
+'''
+Given a string S as the input, the program must print the index of the first non-repeating character in S. If it doesn't exist, the program must print -1 as the output.
+
+Note: The index of S always starts from 0. 
+
+Boundary Condition(s): 2 <= length of S <=100 
+
+Input Format:  The first line contains the string S. 
+Output Format: The first line contains the index of the first non-repeating character in S or -1. 
+
+Example Input/Output 1: 
+
+Input:  skillrack 
+Output: 0 
+
+Explanation: Here s is the first non-repeating character in the string "skillrack". The index of s is 0. Hence the output is 0 
+
+Example Input/Output 2: 
+
+Input:   abbcaac 
+Output: -1
+
+SOLUTION:
+'''
