104bf83f0f373320609561a0d4f8c928e5b2c332
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-05 22:19:02 +0530

    Create Digit - Reverse Pattern.py

---
+++ Digit - Reverse Pattern.py
@@ -0,0 +1,28 @@
+'''
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 0 <= N <= 10^8 
+
+Input Format:  The first line contains N. 
+Output Format: The first N lines contain integers as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  74381
+
+Output: 7 
+        44444444 
+        333 
+        8888 
+        1111111 
+        
+Example Input/Output 2:
+
+Input:  5019 
+Output: 555555555 
+        0 
+        - 
+        99999
+
+SOLUTION:
+'''
