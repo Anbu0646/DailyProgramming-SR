726ffb6ffe1e4ccec69fb2f50daa02bb66a53354
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-06 20:32:47 +0530

    Create Inverted Triangle with Numbers.py

---
+++ Inverted Triangle with Numbers.py
@@ -0,0 +1,25 @@
+'''
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= N <= 50 
+
+Input Format:  The first line contains the value of N. 
+Output Format: The first N lines containing the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1: 
+
+Input:  3
+Output: 1 * 2 * 3 
+        - 5 * 4
+        - - 6 
+        
+Example Input/Output 2: 
+
+Input:  4 
+Output: 1 * 2 * 3 * 4
+        - 7 * 6 * 5 
+        - - 8 * 9 
+        - - - 10
+
+SOLUTION:
+'''
