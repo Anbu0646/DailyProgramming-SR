0759171e0a39a57ddd68ed9a50c935836b048242
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-08 22:17:03 +0530

    Create Odd or Even digits - At least 2.py

---
+++ Odd or Even digits - At least 2.py
@@ -0,0 +1,24 @@
+'''
+The program must accept two integers X and Y as the input. The program must print the integers from X to Y (inclusive of X and Y) having at least two odd digits and at 
+least two even digits as the output. If there is no such integer from X to Y then the program must print -1 as the output.
+
+Boundary Condition(s): 1000 <= X, Y <= 10^8 
+
+Input Format:  The first line contains the values of X and Y separated by a space(s). 
+Output Format: The first line contains the N integers separated by a space.
+
+Example Input/Output 1: 
+
+Input:  3040 3061 
+Output: 3041 3043 3045 3047 3049 3050 3052 3054 3056 3058 3061 
+
+Explanation: Only 3041, 3043, 3045, 3047, 3049, 3050, 3052, 3054, 3056, 3058 and 3061 are having at least two odd digits and at least two even digits in the range from 
+             3040 to 3061. So they are printed as the output. 
+             
+Example Input/Output 2: 
+
+Input:  1130 1139 
+Output: -1
+
+SOLUTION:
+'''
