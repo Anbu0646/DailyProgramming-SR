2f1680303acbb307f1a5397f4dcc62a682ba0b7f
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-11 20:22:19 +0530

    Create K alphabets in Reverse order.py

---
+++ K alphabets in Reverse order.py
@@ -0,0 +1,24 @@
+'''
+The program must accept a lowercase alphabet ch and an integer K as the input. The program must print K alphabets from ch in reverse order. 
+
+Note: If K alphabets are not available from ch then print only available alphabets in reverse order. 
+
+Boundary Condition(s): 1 <= K <= 26 
+
+Input Format:  The first line contains ch and K separated by a space. 
+Output Format: The first line contains alphabets as per the given condition. 
+
+Example Input/Output 1: 
+
+Input:  a 4 
+Output: dcba 
+
+Example Input/Output 2: 
+
+Input:  x 5 
+Output: zyx 
+
+Explanation: There are only three alphabets available from x (including x). Hence those three alphabets are printed in reverse order.
+
+SOLUTION:
+'''
