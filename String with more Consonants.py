e519579c5506bf9514f440fb36efb32e3eb9d394
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-16 11:40:21 +0530

    Create String with more Consonants.py

---
+++ String with more Consonants.py
@@ -0,0 +1,40 @@
+'''
+The program must accept N string values as the input. The program must print the string having maximum number of consonants among the N string values as the output. 
+If more than one string values have the maximum number of consonants then the program must print the first occurring string. 
+
+Note: At least one string will always contain one consonant. 
+
+Boundary Condition(s): 1 <= N <= 100 
+                       1 <= Length of each string <= 100 
+                       
+Input Format:  The first line contains N. The next N lines, each contains a string.
+Output Format: The first line contains the string having maximum number of consonants based on the above conditions.
+
+Example Input/Output 1: 
+
+Input:  3 
+        burger 
+        sandwich 
+        pizza 
+        
+Output: sandwich 
+
+Explanation: The string "burger" has 4 consonants. 
+             The string "sandwich" has 6 consonants. 
+             The string "pizza" has 3 consonants. 
+             Here the string "sandwich" has the maximum number of consonants among the other string values. 
+             Hence the output is sandwich 
+             
+Example Input/Output 2: 
+
+Input:  5
+        CALCIUM 
+        SULPHATE 
+        SODIUM 
+        POTASSIUM 
+        CATS 
+        
+Output: SULPHATE
+
+SOLUTION:
+'''
