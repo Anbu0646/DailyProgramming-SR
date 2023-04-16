0ea5641d61e88fcf25ddd98481fe52dc6c5b5877
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-16 11:54:39 +0530

    Create Second Most Repeating Integer.c

---
+++ Second Most Repeating Integer.c
@@ -0,0 +1,69 @@
+/*
+The program must accept N integers as the input. The program must print the second most repeated integer among the N integers as the output. 
+If there is more than one second most repeated integer then print the first occurring integer among those integers as the output. 
+If there is no such integer then the program must print -1 as the output. 
+
+Boundary Condition(s): 2 <= N <= 1000 
+                       1 <= Each integer value <= 10^5
+                       
+Input Format:  The first line contains the value of N. The second line contains N integers separated by space(s).
+Output Format: The first line contains either the second most repeated integer or -1. 
+
+Example Input/Output 1: 
+
+Input:  6 
+        10 10 19 11 10 19
+       
+Output: 19 
+
+Explanation: 10 is the most repeated integer because it is repeated for 3 times. 
+             So 19 is the second most repeated integer because it is repeated for 2 times which is less than 3. 
+             Hence the output is 19 
+             
+Example Input/Output 2: 
+
+Input:  12 
+        45 49 49 47 46 42 42 44 49 44 40 47 
+        
+Output: 47
+
+SOLUTION:
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int j;
+    scanf("%d", &j);
+    int s[j], d[1000000] = {0}, m=0, n=0;
+  
+    for(int i=0;i<j;i++)
+    {
+        scanf("%d ", &s[i]);
+        d[s[i]]++;
+        if(d[s[i]]>m)
+        {
+            n = m;
+            m = d[s[i]];
+        }
+    }
+  
+    int y=0;
+    for(int i=0;i<j;i++)
+    {
+        if(d[s[i]]==n && n>=2)
+        {
+            printf("%d", s[i]);
+            y = 1;
+            break;
+        }
+    }
+  
+    if(y == 0)
+    {
+        printf("-1");
+    }
+    
+}
