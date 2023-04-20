758cc7046029550ce9740876ea34ec57865f57f0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-20 19:48:57 +0530

    Update Remove all Occurrences  - Unit Digit.c

--- Remove all Occurrences  - Unit Digit.c
+++ Remove all Occurrences  - Unit Digit.c
@@ -28,26 +28,78 @@ SOLUTION:
 */
 
 #include<stdio.h>
+
+int main()
+{
+    unsigned long long int n;
+    scanf("%llu", &n);
+
+    int unit = n%10;
+
+    unsigned long long int dn = 0;
+    
+    while(n)
+    {
+        if(n%10 != unit)
+        {
+            dn = dn*10 + n%10;
+        }
+        n /= 10;
+    }
+    
+    n = 10;
+    
+    while(dn)
+    {        
+        n = n*10 + dn%10;
+        dn /= 10;
+    }
+    
+    printf("llu", n);
+}
+
+
+/*
+#include<stdio.h>
 #include<stdlib.h>
 
 int main()
 {
-    unsigned long long int Num;
-    scanf("%lld", &Num);
-    long long int UnitDigit = Num % 10, Result = 0, Multiply = 1;
+ unsigned long int n, t, c=0, k=0;
+ scanf("%lu", &n);
+ t=n;
     
-    while(Num != 0)
+ while(t>0)
+ {
+  c++; 
+  t/=10;
+ }
+    
+ long int arr[c], i;
+    
+ for(i=c - 1; i>=0; i--)
+ {
+    arr[i] = n%10;
+    n /= 10;
+ }
+    
+ t=0;
+    
+ for(i=0;i<c;i++)
+ {
+    if(arr[i]!=arr[c-1])
     {
-        int CurrentDig = Num % 10;
-        if(CurrentDig != UnitDigit)
-        {
-            Result += CurrentDig * Multiply;
-            Multiply *= 10;
-        }
-    Num /= 10;
+      t = (t*10)+arr[i];
+      k = 1;
     }
+ }
     
-    printf("%lld", (Result == 0)?-1:Result);
+ if(t!=0)
+    printf("%lu", t);
+ else if(k==1)
+   printf("0");
+ else
+   printf("-1");
     
-    return 0;
-}
+ return 0;
+}*/
