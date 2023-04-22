8e4105a8998da27a3959ef99026dcb128015c362
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-04-22 22:05:48 +0530

    Create Interlace factors.c

---
+++ Interlace factors.c
@@ -0,0 +1,25 @@
+/*
+The program must accept two integers X and Y as the input. 
+The program must print the factor(s) of X interlaced with the factor(s) of Y in ascending order as the output. 
+If some factors of X or Y are not having their pairs to interlace then print those factors in ascending order as the output. 
+
+Boundary Condition(s): 1 <= X, Y <= 10^8 
+
+Input Format:  The first line contains the values of X and Y separated by a space. 
+Output Format: The first line contains the factors of X and Y separated by a space as per the given conditions. 
+
+Example Input/Output 1: 
+
+Input:  100 24 
+Output: 1 1 2 2 4 3 5 4 10 6 20 8 25 12 50 24 100 
+
+Explanation: The factors of 100 are 1 2 4 5 10 20 25 50 and 100. The factors of 24 are 1 2 3 4 6 8 12 and 24. So they are printed in interlaced manner as given below. 
+             1 1 2 2 4 3 5 4 10 6 20 8 25 12 50 24 100 
+             
+Example Input/Output 2: 
+
+Input:  50 121 
+Output: 1 1 2 11 5 121 10 25 50
+
+SOLUTION:
+*/
