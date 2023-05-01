490d20474ee427af13393a8ed99ec7a75c43a51a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-01 11:54:52 +0530

    Create Alphabet Mirror Image.c

---
+++ Alphabet Mirror Image.c
@@ -0,0 +1,27 @@
+/*
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= N <= 26 
+
+Input Format:  The first line contains N. 
+Output Format: The first N lines contain the pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  3 
+
+Output: a b c * c b a
+        a b * * * b a 
+        a * * * * * a
+        
+Example Input/Output 2: 
+
+Input:  4 
+
+Output: a b c d * d c b a 
+        a b c * * * c b a
+        a b * * * * * b a 
+        a * * * * * * * a
+
+SOLUTION:
+*/
