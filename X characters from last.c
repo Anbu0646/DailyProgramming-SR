c1e6ac6c482bd4f75783fd3d60ac240a66cdf3e0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-03 22:25:41 +0530

    Create X characters from last.c

---
+++ X characters from last.c
@@ -0,0 +1,42 @@
+/*
+The program must accept a string S and an integer X as the input. The program must print every X characters from the last in the string S on each line as the output. 
+If there is no X characters to print then print the remaining characters as it is. 
+
+Boundary Condition(s):  2 <= Length of S <= 10000
+                        1 <= X <= Length of S 
+
+Input Format:   The first line contains the string S.
+                The second line contains the integer X. 
+
+Output Format:  The lines contain X characters on each line as per the given conditions. 
+
+Example Input/Output 1: 
+
+Input: 
+
+apples 
+3 
+
+Output: 
+
+les 
+app 
+
+Explanation:  The last 3 characters are l e and s. So they are printed in the first line. 
+              The next 3 characters from the last are a p and p. So they are printed in the second line. 
+
+Example Input/Output 2: 
+
+Input: 
+
+skillrack 
+4 
+
+Output: 
+
+rack 
+kill 
+s
+
+SOLUTION:
+*/
