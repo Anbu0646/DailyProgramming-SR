77e2b6c57dc1ce2be46e711e45df08ccd574bb3c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-10 11:02:26 +0530

    Update Reverse Last X integers - Matrix.c

--- Reverse Last X integers - Matrix.c
+++ Reverse Last X integers - Matrix.c
@@ -62,3 +62,36 @@ Output:
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int r, c, k;
+    scanf("%d%d", &r, &c);
+    int arr[r][c];
+  
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+  
+    scanf("%d",&k);
+  
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c-k; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        for(int j = c-1; j >=c-k; j--)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
