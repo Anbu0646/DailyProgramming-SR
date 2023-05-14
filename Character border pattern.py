9efd54a4dae7264b1322224db224c5a0cc2818ec
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-14 22:05:21 +0530

    Create Character border pattern.py

---
+++ Character border pattern.py
@@ -0,0 +1,35 @@
+'''
+The program must accept an odd integer N and a character CH as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= N <= 99 
+
+Input Format:  The first line contains the values of N and CH separated by a space. 
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input: 5 #
+
+Output:
+
+# # # # # # # # # 
+- # 5 4 3 2 1 # 
+- - # 5 4 3 #
+- - - # 2 # 
+- - - - # 
+
+Example Input/Output 2: 
+
+Input:  7 @ 
+
+Output: 
+@ @ @ @ @ @ @ @ @ @ @
+- @ 7 6 5 4 3 2 1 @ 
+- - @ 7 6 5 4 3 @
+- - - @ 2 1 7 @
+- - - - @ 6 @ 
+- - - - - @
+
+SOLUTION:
+'''
