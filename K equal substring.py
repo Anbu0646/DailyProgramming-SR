bf24e926d3d99a1f08e2f5e93de59bb83c5af3e3
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-16 22:14:06 +0530

    Create K equal substring.py

---
+++ K equal substring.py
@@ -0,0 +1,32 @@
+'''
+The program must accept a string S and an integer K as the input. The program must print yes if the string can be rearranged and divided into K equal substring values. Else the program must print no as the output. 
+
+Note: S contains lowercase alphabets. 
+
+Boundary Condition(s): 
+
+1 <= Length of S <= 1000 
+1 <= K <= 50 
+
+Input Format:  The first line contains S and K separated by a space.
+
+Output Format: The first line contains yes or no. 
+
+Example Input/Output 1: 
+
+Input:  rtraddttrada 3 
+
+Output: yes 
+
+Explanation: 
+
+The string can be rearranged as tradtradtrad and divided into three equal substring values. Hence yes is printed. 
+
+Example Input/Output 2: 
+
+Input:  aazzez 2 
+
+Output: no
+
+SOLUTION:
+'''
