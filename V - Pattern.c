710fe4272af2cb708c901879f5c4a6b97f03819e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-05-21 22:11:09 +0530

    Create V - Pattern.c

---
+++ V - Pattern.c
@@ -0,0 +1,38 @@
+/*
+The program must accept a string S as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Note: The length of S is always odd. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format: 
+
+The first line contains the string S. 
+
+Output Format: 
+
+The list of lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  apple 
+
+Output: 
+
+apple 
+*ppl* 
+**p** 
+
+Example Input/Output 2: 
+
+Input:  program 
+
+Output: 
+
+program 
+*rogra* 
+**ogr** 
+***g***
+
+SOLUTION:
+*/
