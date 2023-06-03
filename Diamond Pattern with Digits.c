10769df1a5e2e2b417e949e5e1c3787d862fd2a5
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-03 12:32:31 +0530

    Update Diamond Pattern with Digits.c

--- Diamond Pattern with Digits.c
+++ Diamond Pattern with Digits.c
@@ -53,3 +53,80 @@ Output:
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i, j, n, l, s, c = 0;
+    scanf("%d", &n);
+  
+    for(i = -n+1; i<=n-1; i++)
+    {
+        if(i<0) l = -i;
+        if(i>=0) l = +i;
+      
+        for(s=0; s<l; s++)
+        {
+            printf("-");
+        }
+      
+        printf("<");
+      
+        for(j=0; j<2*(n-l)-1; j++)
+        {
+            printf("%d", c++%10);
+        }
+      
+        printf(">\n");
+    }
+}
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+ int n,x=1,z=1;
+ scanf("%d",&n);
+ int l=(n*2)-1,k=0;
+ for(int i=0;i<=l/2;i++)
+ {
+    for(int j=0;j<n-x;j++)
+    {
+      printf("-"); 
+    }
+    x++;
+    printf("<");
+    for(int j=0;j<z;j++)
+    {
+      printf("%d",k++);
+      if(k==10)
+         k=0;
+    }
+    z=z+2;
+    printf(">\n");
+ }
+ x=1,z=l-2;
+ for(int i=0;i<n-1;i++)
+ {
+    for(int j=0;j<x;j++)
+    {
+      printf("-");  
+    }
+    x++;
+    printf("<");
+    for(int i=0;i<z;i++)
+    {
+       printf("%d",k++);
+       if(k==10)
+         k=0;
+    }
+    z=z-2;
+    printf(">\n");
+ }
+ return 0;
+}
+*/
