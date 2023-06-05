f7b419493e15b8b67e54329ac75a1217907522e1
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-05 22:07:47 +0530

    Create Up Arrow Pattern.c

---
+++ Up Arrow Pattern.c
@@ -0,0 +1,40 @@
+/*
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 2 <= N <= 100 
+
+Input Format: 
+
+The first line contains the value of N.
+ 
+Output Format: 
+
+The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input: 
+
+3 
+
+Output: 
+
+- - 3 
+- 3 2 3 
+3 2 1 2 3 
+
+Example Input/Output 2: 
+
+Input: 
+
+4 
+
+Output: 
+
+- - - 4 
+- - 4 3 4 
+- 4 3 2 3 4 
+4 3 2 1 2 3 4
+
+SOLUTION:
+*/
