d96ad61a1fd577ae0485960ce2bc673635fbe60a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-06 09:23:13 +0530

    Update and rename Up Arrow Pattern.c to Up Arrow Layer Pattern.c

--- Up Arrow Layer Pattern.c
+++ Up Arrow Layer Pattern.c
@@ -38,3 +38,31 @@ Output:
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=1; j<n - i; j++)
+        {
+            printf("- ");
+        }
+     
+        for(int j=n; j>=n - i; j--)
+        {
+            printf("%d ", j);
+        }
+     
+        for(int j=n - i + 1; j<=n; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
