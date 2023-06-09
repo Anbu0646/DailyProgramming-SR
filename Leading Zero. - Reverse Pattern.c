75d6526e296fb6d8a16ca23ed9c3d06db37c8f42
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-09 22:11:04 +0530

    Update and rename Reverse the Leading Zero.c to Leading Zero. - Reverse Pattern.c

--- Leading Zero. - Reverse Pattern.c
+++ Leading Zero. - Reverse Pattern.c
@@ -34,3 +34,34 @@ Output:
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d" , &n);
+    for(int i=1; i<=n; printf("\n") , i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(j <= (n - i))
+            {
+                printf("0 ");
+            }
+            else
+            {
+                printf("%d ", (n - j + 1));
+            }
+        }
+    }
+}
+
+/*
+n = int(input())
+m = [0]*n
+for i in range(1,n+1):
+    m[-i] = i
+    print(*m)
+*/
