e8c42a95c90cc24bfe5b98c5f74afc4e3ffc72eb
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-14 22:06:20 +0530

    Create Maximum of number of characters between two successive alphabets.c

---
+++ Maximum of number of characters between two successive alphabets.c
@@ -0,0 +1,32 @@
+/*
+
+The program must accept the value of a string S as the input. 
+The program must print the maximum number of alphabets between any two consecutive same alphabets in the string as the output. 
+If there is no such repeated alphabet, then the program must print -1 as the output. 
+
+Note: The string S contains only lowercase alphabets. 
+
+Boundary Condition(s): 3 <= Length of String S <= 99 
+
+Input Format: The first line contains the value of S. 
+
+Output Format: The first line contains the maximum number of characters between two same alphabets. 
+
+Example Input/Output 1: 
+
+Input:  helloh 
+
+Output: 4 
+
+Explanation: 
+
+There are two repeated alphabets l and h. The number of alphabets between l is 0. The number of alphabets between h is 4. Hence 4 is printed. 
+
+Example Input/Output 2: 
+
+Input: xyz 
+
+Output: -1
+
+SOLUTION:
+*/
