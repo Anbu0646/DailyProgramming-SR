3ed36f90e151412363296b4c9405fcfea9e1e3db
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-15 11:25:54 +0530

    Create Alternate Characters in S1 and S2.c

---
+++ Alternate Characters in S1 and S2.c
@@ -0,0 +1,35 @@
+/*
+The program must accept two string values S1 and S2 as the input. 
+The program must print the first character in S1, 
+then the program must print the second character in S2, then the program must print the third character in S1, then the program must print the fourth character in S2 and so on. 
+
+Note: Both the string values are always having the same length. 
+
+Boundary Condition(s): 1 <= Length of S1 and S2 <= 100 I
+
+nput Format:  The first line contains the string S1. The second line contains the string S2. 
+
+Output Format: The first line contains the modified string.
+
+
+Example Input/Output 1:
+
+Input:  lemon apple 
+Output: lpmln 
+
+Explanation: 
+
+The first character in the sting lemon is l. 
+The second character in the sting apple is p. 
+The third character in the sting lemon is m. 
+The fourth character in the string apple is l. 
+The fifth character in the string lemon is n. 
+Hence the output is lpmln 
+
+Example Input/Output 2: 
+
+Input:  copy past 
+Output: capt
+
+SOLUTION:
+*/
