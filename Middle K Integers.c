bd45dbc0e5e02d58781d1ac23183b959ad91860d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-19 20:16:47 +0530

    Create Middle K Integers.c

---
+++ Middle K Integers.c
@@ -0,0 +1,49 @@
+/*
+The program must accept N integers and an integer K as the input. The program must print middle K integers in the reverse order in the N integers as the output. 
+
+Note: Both the integers N and K are odd or both the integers are even. 
+
+Boundary Condition(s): 
+
+1 <= N <= 10^5 
+1 <= K <= N 
+
+Input Format: 
+
+The first line contains the two integers N and K separated by a space. 
+The second line contains N integers separated by space(s). 
+
+Output Format: 
+
+The first line contains middle K integers in the reverse order. 
+
+Example Input/Output 1: 
+
+Input: 
+
+13 5 
+20 10 30 40 5 15 50 25 35 130 110 120 45 
+
+Output: 
+
+35 25 50 15 5 
+
+Explanation: 
+
+The middle 5 integers are 5, 15, 50, 25 and 35. 
+In the reverse order, the values are 35, 25, 50, 15, 5. 
+Hence the output is 35 25 50 15 5 
+
+Example Input/Output 2: 
+
+Input: 
+
+10 4 
+89 12 15 50 65 13 52 28 15 80 
+
+Output: 
+
+52 13 65 50
+
+SOLUTION:
+*/
