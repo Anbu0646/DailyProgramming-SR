1353b87090c8f5795a3358778545a7c7efa3f85c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-25 22:14:55 +0530

    Create Count of a and b.c

---
+++ Count of a and b.c
@@ -0,0 +1,22 @@
+/*
+The program must accept a string which will contain only a and b and generate the output based on the continuous count of a or b as described in Example Input/Output section. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format:  The first line contains the string. 
+Output Format: The first line contains the count of a and b as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  aababbaaabbbb 
+
+Output: 2a 1b 1a 2b 3a 4b 
+
+Example Input/Output 2: 
+
+Input:  bbb 
+
+Output: 3b
+
+SOLUTION:
+*/
