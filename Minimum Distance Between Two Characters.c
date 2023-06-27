7a98a3e62d9043021de01152c412aee5d2113ec6
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-27 22:14:24 +0530

    Create Minimum Distance Between Two Characters.c

---
+++ Minimum Distance Between Two Characters.c
@@ -0,0 +1,44 @@
+/*
+The program must accept a string S and two characters as CH1 and CH2 as the input. 
+The program must print the minimum distance between CH1 and CH2 in the string S as the output. 
+
+Note:  The characters CH1 and CH2 must be in same order as given in the input for measuring the distance. 
+
+Boundary Condition(s): 2 <= Lenght of S <= 100 
+
+Input Format: 
+
+The first line contains the string S. 
+The second line contains two characters separated by a space. 
+
+Output Format: 
+
+The first line contains the minimum distance between two characters in S. 
+
+Example Input/Output 1: 
+
+Input: 
+
+hello  world 
+o  l 
+
+Output: 1
+
+Explanation: 
+
+The distance between o and l in the hello world is 4. 
+The distance between o and l in the hello world is 1. 
+Here the minimum distance is 1. 
+Hence the output is 1 .
+
+Example Input/Output 2: 
+
+Input: 
+
+may  i  know  your  good  name  
+a  m 
+
+Output: 0
+
+SOLUTION:
+*/
