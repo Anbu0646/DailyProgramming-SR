640c0dcebe2542ca7267063645db0003a865fcbf
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-06-30 22:07:58 +0530

    Create Consonant Substring values.c

---
+++ Consonant Substring values.c
@@ -0,0 +1,32 @@
+/*
+The program must accept a string S as the input. 
+The program must split the string S into substring values by removing all the vowels in the string S. 
+Then the program must print all the substring values in the order of occurrence separated by a hyphen. 
+
+Note: At least one consonant is always present in the string S. 
+
+Boundary Condition(s):  1 <= Length of S <= 100 
+
+Input Format:  The first line contains the string S. 
+
+Output Format: The first line contains all the substring values in the order of occurrence separated by a hyphen as per the given condition. 
+
+Example Input/Output 1: 
+
+Input:  Forests 
+
+Output: F-r-sts 
+
+Explanation: 
+
+There are three substring values after removing the vowels in the string "Forests". 
+Hence the output is F-r-sts. 
+
+Example Input/Output 2: 
+
+Input:  ACTIVITIES 
+
+Output: CT-V-T-S
+
+SOLUTION:
+*/
