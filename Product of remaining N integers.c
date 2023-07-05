b7097b0247321cb6424ed3a564b5b3da243e9758
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-05 22:23:35 +0530

    Update Product of remaining N integers.c

--- Product of remaining N integers.c
+++ Product of remaining N integers.c
@@ -46,3 +46,30 @@ Output: 0 7
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        int p = 1;
+        for(int j=0; j<n; j++)
+        {
+            if(i != j)
+            {
+                p=p*a[j];
+            }
+        }
+        printf("%d ", p);
+    }
+
+}
