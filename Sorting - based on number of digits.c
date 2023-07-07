cae4aba2eda755536fd16a77d891bc73026f8a10
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-07 22:16:21 +0530

    Create Sorting - based on number of digits.c

---
+++ Sorting - based on number of digits.c
@@ -0,0 +1,49 @@
+/*
+The program must accept N integers as the input. 
+The program prints yes if the integers are sorted in ascending order based on the number of digits in it.
+Else the program prints no as the output. 
+
+Boundary Conditions: 
+
+1 <= N <= 100
+1 <= Each integer value <= 10^8 
+
+Input Format: 
+
+The first line contains the value of N. 
+The second line contains the N integers separated by space(s). 
+
+Output Format: 
+
+The first line contains either yes or no. 
+
+Example Input/Output 1: 
+
+Input: 
+
+5 
+9  12  56  20  850 
+
+Output: yes 
+
+Explanation: 
+
+The number of digits in 9 is 1. 
+The number of digits in 12 is 2. 
+The number of digits in 56 is 2. 
+The number of digits in 20 is 2. 
+The number of digits in 850 is 3. 
+Hence the integers are sorted in ascending order based on the number of digits. 
+So yes is printed as the output. 
+
+Example Input/Output 2: 
+
+Input: 
+
+6 
+20  50  18  110  99  147 
+
+Output: no
+
+SOLUTION:
+*/
