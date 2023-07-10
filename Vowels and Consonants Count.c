9d3bf157fcc152da93fda407a6ac1565c7f6768a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-10 22:25:15 +0530

    Create Vowels and Consonants Count.c

---
+++ Vowels and Consonants Count.c
@@ -0,0 +1,50 @@
+/*
+The program must accept three string values S1, S2 and S3 containing only alphabets. 
+For any given position, if the count of vowels is more than the count of consonants then the program must print V. 
+Else if the count of vowels is less than the count of consonants then the program must print C. Else the program must print E as the output. 
+
+Boundary Condition(s): 1 <= Length of S1, S2, S3 <= 100 
+
+Input Format: 
+
+The first line contains S1. 
+The second line contains S2. 
+The third line contains S3. 
+
+Output Format: 
+
+The first line contains the string value. 
+
+Example Input/Output 1: 
+
+Input: 
+
+elephant 
+orange 
+mango 
+
+Output: VCVCCVCC 
+
+Explanation: 
+
+1st postion - 2 vowels and 1 consonant. So V is printed. 
+2nd postion - 1 vowel and 2 consonants. So C is printed. 
+3rd postion - 2 vowels and 1 consonant. So V is printed. 
+4th postion - 3 consonants. So C is printed. 
+5th postion - 1 vowel and 2 consonants. So C is printed. 
+6th postion - 2 vowels. So V is printed. 
+7th postion - 1 consonant. So C is printed. 
+8th postion - 1 consonant. So C is printed. 
+
+Example Input/Output 2: 
+
+Input: 
+
+apple 
+create 
+profess 
+
+Output: CCVCVEC
+
+SOLUTION:
+*/
