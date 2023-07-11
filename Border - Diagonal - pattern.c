93485842d5694bca8ef0108a36219b4ecaa32fd4
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-11 22:10:32 +0530

    Create Border - Diagonal - pattern.c

---
+++ Border - Diagonal - pattern.c
@@ -0,0 +1,41 @@
+/*
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 4 <= N <= 100 
+
+Input Format:  The first line contains the value of N. 
+
+Output Format: The first N lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input: 7 
+
+Output: 
+
+# * * * * * # 
+* * - - - * * 
+* - * - * - * 
+* - - * - - * 
+* - * - * - * 
+* * - - - * * 
+# * * * * * # 
+
+Example Input/Output 2: 
+
+Input: 8 
+
+Output: 
+
+# * * * * * * # 
+* * - - - - * * 
+* - * - - * - * 
+* - - * * - - * 
+* - - * * - - * 
+* - * - - * - * 
+* * - - - - * * 
+# * * * * * * #
+
+SOLUTION:
+*/
