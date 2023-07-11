382f8b8af55dc4226bdbdb061d88a5ef37d01ed8
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-11 09:41:20 +0530

    Update Numbers and Asterisks - V pattern.c

--- Numbers and Asterisks - V pattern.c
+++ Numbers and Asterisks - V pattern.c
@@ -33,6 +33,42 @@ Output:
 
 SOLUTION:
 */
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{                           
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++)
+    {
+        int x = 1;
+        
+        for(int j=0; j<i; j++)
+        {
+            printf("*");
+        }
+        
+        for(int k=0; k<n - i; k++)
+        {
+            if(k == n - i - 1)
+               printf("%d",x);
+            else
+               printf("%d*", x);
+               x++;
+        }
+        
+        for(int h=0; h<i; h++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+/*
+Aliter:
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -71,3 +107,47 @@ int main()
         printf("\n");
     }
 }
+
+
+PY3:
+
+n=int(input())
+k=[str(i) for i in range(1,n+1)]
+c=0
+for i in range(n,0,-1):
+    print("*"*c+"*".join(k[:i])+"*"*c) 
+    c+=1
+
+
+a=int(input())
+s=1
+b=(a*2)-1
+for i in range(0,a):
+    for j in range(0,i):
+        print("*",end="")
+    for k in range(0,b-(i*2)):
+        if(k%2==0):
+            print(s,end="")
+            s+=1
+        else:
+            print("*",end="")
+    for m in range(0,i):
+        print("*",end="")
+    s=1
+    print()
+
+n=int(input())
+lst=[]
+for i in range(n):
+    temp=[]
+    for j in range(1,n+1-i):
+        temp.append(j)
+    lst.append(temp)
+for i in range(n):
+    for j in range(i):
+        print('*',end="")
+    print(*lst[i],sep="*",end="")
+    for j in range(i):
+        print('*',end="")
+    print()
+*/
