8796796597a02350cb2bbd5683825ddc3bab8c4b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-27 22:05:12 +0530

    Create Parallelogram pattern.c

---
+++ Parallelogram pattern.c
@@ -0,0 +1,35 @@
+/*
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 100 
+
+Input Format:  The first line contains N. 
+
+Output Format: The first N lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1:
+
+Input:  4 
+
+Output: 
+
+* * * * 
+- * 1 2 *
+- - * 3 4 * 
+- - - * * * * 
+
+Example Input/Output 2: 
+
+Input:  5  
+
+Output: 
+
+* * * * * 
+- * 1 2 3 * 
+- - * 4 5 6 
+* - - - * 7 8 9 
+* - - - - * * * * *
+
+SOLUTION:
+*/
