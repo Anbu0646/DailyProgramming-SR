4d7a3479080cccd6f61177e78194078918881ea2
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-28 22:07:12 +0530

    Update Parallelogram pattern.c

--- Parallelogram pattern.c
+++ Parallelogram pattern.c
@@ -33,3 +33,32 @@ Output:
 
 SOLUTION:
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n, k = 1;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            printf("- ");
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(i==0 || j==0 || i==n-1 || j==n-1)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("%d ",k++);
+            }
+        }
+        printf("\n");
+    }
+
+}
