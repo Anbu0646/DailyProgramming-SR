336311db8ffc97edfa479469488c6bbcadc26b90
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-28 22:02:41 +0530

    Create String - Hidden triangle pattern.c

---
+++ String - Hidden triangle pattern.c
@@ -0,0 +1,36 @@
+/*
+The program must accept a string S where the length of S is always odd as the input.
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= Length of S <= 99 
+
+Input Format:  The first line contains S. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  chicken 
+
+Output: 
+
+kencchi 
+enickch 
+nhickec 
+chicken 
+
+Example Input/Output 2: 
+
+Input:  ABCDEFGHI 
+
+Output: 
+
+FGHIEABCD 
+GHIDEFABC 
+HICDEFGAB 
+IBCDEFGHA 
+ABCDEFGHI
+
+SOLUTION:
+
+*/
