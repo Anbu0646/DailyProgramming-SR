b647af0a67a29c3db964fed0b7627e84680c0afe
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-07-29 22:13:54 +0530

    Update String - Hidden triangle pattern.c

--- String - Hidden triangle pattern.c
+++ String - Hidden triangle pattern.c
@@ -34,3 +34,47 @@ ABCDEFGHI
 SOLUTION:
 
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  char gg[1000];
+  scanf("%s", gg);
+  int m = strlen(gg);
+  int y = (m/2) - 1;
+  int z = (m/2) + 1;
+  
+  while(y>=0)
+  {
+    for(int i=z; i<m; i++)
+    {
+        printf("%c", gg[i]);
+    }
+    for(int i=y + 1; i<z; i++)
+    {
+        printf("%c", gg[i]);
+    }
+    for(int i=0; i<=y; i++)
+    {
+        printf("%c", gg[i]);
+    }
+    y--;
+    z++;
+    printf("\n");
+  }
+  
+  printf("%s", gg);
+}
+
+/*
+PY3:
+
+ac = input().strip()
+mid = len(ac)//2
+
+for i in range(len(ac)//2 + 1):
+    print(ac[-mid:] + ac[mid:-mid] + ac[:mid])
+    mid -= 1
+*/
