78ddac0b5c9eba03efe1c52ad876e6b1572a04f7
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-01 21:12:09 +0530

    Update Interchange quadrants.py

--- Interchange quadrants.py
+++ Interchange quadrants.py
@@ -110,3 +110,56 @@ for i in range(N):
     for j in range(N):
         print(Mat_2[i][j], end=" ")
     print()
+'''
+CPP:
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n,t;
+    cin>>n;
+    int a[n][n],b[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>b[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<n/2 && j>=n/2){
+                t=a[i][j];
+                a[i][j]=b[i][j];
+                b[i][j]=t;
+            }
+            else if(i>=n/2 && j<n/2){
+                t=a[i][j];
+                a[i][j]=b[i][j];
+                b[i][j]=t;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<b[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+'''
