40682a20b9eeccf02e12827400b6a3a0969f644d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-02 22:24:56 +0530

    Update Digital Sum - Compress Integer.cpp

--- Digital Sum - Compress Integer.cpp
+++ Digital Sum - Compress Integer.cpp
@@ -32,3 +32,74 @@ Output: 357
 
 SOLUTION:
 */
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int sum(int n)
+{
+    int ans = 0;
+    while(n)
+    {
+        ans += n%10;
+        n /= 10;
+    }
+    return ans;
+}
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    int n = s.length();
+
+    int val=0;
+
+    for(int i=0; i<s.length() - 1; i++)
+    {
+        val *= 10;
+        int x = (s[i] - '0') + (s[i + 1] - '0');
+    
+        x = sum(x);
+        val += x;
+    }
+  
+    cout<<val;
+}
+
+/*
+C:
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   char s[1000];
+   scanf("%s", s);
+   int l = strlen(s) - 1;
+   for(int i=0; i<l; i++)
+   {
+       int l = (s[i] - '0') + (s[i + 1] - '0');
+       int s = 0;
+       while(l > 0)
+       {
+           s = s + (l % 10);
+           l = l/10;
+       }
+       printf("%d",s);
+   }
+}
+
+
+PY3:
+
+s = input().strip()
+ans = ""
+for i in range(len(s) - 1):
+    k = int(s[i]) + int(s[i + 1])
+    if(k > 9):
+        k = k % 10 + (k // 10 % 10)
+    ans += str(k)
+print(ans)
+*/
