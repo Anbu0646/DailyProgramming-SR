6b25ce361b73b60ed4e1f3471a642b965a64170d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-04 22:15:02 +0530

    Create Bits Count - Y to X.c

---
+++ Bits Count - Y to X.c
@@ -0,0 +1,31 @@
+/*
+The program must accept two integers X and Y where X is always greater than or equal to Y. 
+The program must print the count of bits C that must be changed in Y so that it becomes X. 
+
+Boundary Condition(s):  1 <= Y <= X <= 10^8 
+
+Input Format:  The first line contains X and Y separated by a space. 
+
+Output Format: The first line contains an integer value representing C. 
+
+Example Input/Output 1: 
+
+Input:  5 4 
+
+Output: 1 
+
+Explanation: 
+
+The binary representation of X=5 is 101. 
+The binary representation of Y=4 is 100. 
+Changing the last bit we can get 5 which is X. 
+Hence the output is 1. 
+
+Example Input/Output 2: 
+
+Input:  10 6
+
+Output: 2
+
+SOLUTION:
+*/
