e2bff58b91765b7c9c731782ffa7f3bfb33fca34
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-07 22:13:59 +0530

    Create Swap first occurring odd and even digit.cpp

---
+++ Swap first occurring odd and even digit.cpp
@@ -0,0 +1,25 @@
+/*
+The program must accept an integer X and swap the first occurring odd and even digit. 
+The integer X will have at least one odd and one even digit. 
+Consider 0 (zero) also as an even digit. 
+
+Boundary Condition(s): 10 <= X <= 10^8 
+
+Input Format:  The first line contains X. 
+
+Output Format: The first line contains the modified integer value. 
+
+Example Input/Output 1: 
+
+Input:  12345 
+
+Output: 21345 
+
+Example Input/Output 2: 
+
+Input:  106 
+
+Output: 16
+
+SOLUTION:
+*/
