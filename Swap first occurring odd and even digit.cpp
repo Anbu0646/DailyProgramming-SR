1ae414d987ed08191ea69e88e4ab83c1c5b76686
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-08 22:10:04 +0530

    Update Swap first occurring odd and even digit.cpp

--- Swap first occurring odd and even digit.cpp
+++ Swap first occurring odd and even digit.cpp
@@ -23,3 +23,70 @@ Output: 16
 
 SOLUTION:
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    int i = 0;
+    
+    while(s[i] && s[i] % 2 == 0)
+    {
+        i++;
+    }
+  
+    int j = 0;
+  
+    while(s[j] && s[j] % 2)
+    {
+        j++;
+    }
+  
+    swap(s[i], s[j]);
+    cout<<stol(s);
+}
+
+/*
+C:
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int n,i,x,a,b,e=0,o=0;
+char s[1000],c;
+scanf("%s%n",s,&n);
+for(i=0;i<n;i++){
+    x=s[i]-'0';
+    if(x%2==0 && e==0){
+        a=i;
+        e=1;
+    }
+    if(x%2!=0 && o==0){
+        b=i;
+        o=1;
+    }
+}
+c=s[a];
+s[a]=s[b];
+s[b]=c;
+printf("%d",atoi(s));
+
+PY3:
+x=input().strip()
+x=list(x)
+for i in range(len(x)):
+    if int(x[i])%2==0:
+        break
+for j in range(len(x)):
+    if int(x[j])%2!=0:
+        break
+x[i],x[j]=x[j],x[i]
+x="".join(x)
+print(int(x))
+}
+*/
