a8a4ff82d22ea289ca9b1cba1e48649a0486dfd9
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-10 22:25:53 +0530

    Create Oldest Date.cpp

---
+++ Oldest Date.cpp
@@ -0,0 +1,56 @@
+/*
+The program must accept N dates in the format DD/MM/YYYY as the input. 
+The program must print the historically oldest date among the N dates as the output. 
+
+Note: All the N dates are valid. 
+
+Boundary Condition(s): 2 <= N <= 100 
+
+Input Format: 
+
+The first line contains N. 
+The next N lines each contain a date in the format DD/MM/YYYY. 
+
+Output Format: 
+
+The first line contains the historically oldest date among the N dates. 
+
+Example Input/Output 1: 
+
+Input: 
+
+5 
+02/04/2000 
+14/11/1996 
+02/11/1996 
+24/12/1996 
+22/12/2018 
+
+Output: 02/11/1996 
+
+Explanation: 
+
+The oldest date among the 5 dates is 02/11/1996. 
+Hence the output is 02/11/1996 
+
+Example Input/Output 2: 
+
+Input: 
+
+4 
+29/01/1997
+13/07/1996 
+24/01/1999 
+30/08/1990 
+
+Output:30/08/1990
+
+SOLUTION:
+*/
+
+/*
+PY3:
+
+
+C:
+*/
