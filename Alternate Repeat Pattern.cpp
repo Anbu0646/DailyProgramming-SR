277d04cac1a4195a22be3c61aa6db2a92443e5b0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-11 19:47:32 +0530

    Update and rename Alternate Repeat Pattern.c to Alternate Repeat Pattern.cpp

--- Alternate Repeat Pattern.cpp
+++ Alternate Repeat Pattern.cpp
@@ -30,6 +30,37 @@ Output:
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int ntr;
+    cin>>ntr;
+    for(int i=1; i<=ntr/2; i++)
+    {
+        for(int j=1; j<=ntr + 1; j++)
+        {
+            if(j<=i)
+            {
+                cout<<i;
+            }
+            else
+            {
+                cout<<ntr - i + 1;
+            }
+        }
+    
+    
+    printf("\n");    
+    }
+    return 0;
+}
+
+/*
+C:
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -55,7 +86,7 @@ int main()
     }
 }
 
-/*
+
 PY3:
 
 */
