3d131ed2cad17cfe5ee299893aff5d760baa9591
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-14 22:19:15 +0530

    Create Integers - Positive and Negative.cpp

---
+++ Integers - Positive and Negative.cpp
@@ -0,0 +1,47 @@
+/*
+The program must accept N integers as the input. 
+The program must print the integers which have both positive and negative values in the N integers as the output. 
+The integers must be printed in the same order as given in the input.
+
+Note:
+
+- There will be at least one integer with both positive and negative values. 
+- All the integers are always a nonzero value. 
+
+Boundary Condition(s): 
+
+2 <= N <= 100 
+-1000 <= Each integer value <= 1000 
+
+Input Format: 
+
+The first line contains N.
+The second line contains N integers separated by a space. 
+
+Output Format: The first line contains a list of integers separated by a space as per the given conditions. 
+
+Example Input/Output 1: 
+
+Input: 
+
+7 
+10 -45 26 -10 45 10 31 
+
+Output: 10 -45 -10 45 10 
+
+Explanation: 
+
+The integers 10 and 45 are present in both positive and negative values. 
+Hence the output is 10 -45 -10 45 10 
+
+Example Input/Output 2: 
+
+Input: 
+
+6
+11 12 26 -12 97 11 
+
+Output: 12 -12
+
+SOLUTION:
+*/
