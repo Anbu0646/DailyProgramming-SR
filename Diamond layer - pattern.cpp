fd16e5ebda63503e71bb7b4c648c8b2bfd57a9e2
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-22 20:00:35 +0530

    Create Diamond layer - pattern.cpp

---
+++ Diamond layer - pattern.cpp
@@ -0,0 +1,51 @@
+/*
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 2 <= N <= 100 
+
+Input Format:  The first line contains N. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  5 
+
+Output: 
+
+####*#### 
+###*-*### 
+##*-*-*## 
+#*-*-*-*# 
+*-*-*-*-* 
+#*-*-*-*# 
+##*-*-*## 
+###*-*### 
+####*#### 
+
+Example Input/Output 2: 
+
+Input:  8 
+
+Output:
+
+#######*####### 
+######*-*###### 
+#####*-*-*##### 
+####*-*-*-*####
+###*-*-*-*-*### 
+##*-*-*-*-*-*## 
+#*-*-*-*-*-*-*#
+*-*-*-*-*-*-*-* 
+#*-*-*-*-*-*-*# 
+##*-*-*-*-*-*## 
+###*-*-*-*-*### 
+####*-*-*-*####
+#####*-*-*#####
+######*-*###### 
+#######*#######
+
+
+SOLUTION:
+*/
