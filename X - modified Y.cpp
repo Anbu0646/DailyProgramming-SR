effec4be340c8138d7ceabc3dbaececc3e8cf23c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-08-31 12:24:53 +0530

    Update X - modified Y.cpp

--- X - modified Y.cpp
+++ X - modified Y.cpp
@@ -32,6 +32,116 @@ Output: 4444
 SOLUTION:
 */
 
-/*
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    for(int i=0; i<s.length(); i++)
+    {
+        int val = '9' - s[i];
+        if(val < 5)
+        {
+            if(val == 0 && i == 0)
+            {
+                cout<<9;
+            }
+            else
+                cout<<val;
+        }
+        else
+        {
+            cout<<s[i];
+        }
+    }
+}
+
+/*
+C:
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char str[1001];
+    scanf("%s", str);
+    int flag = 0;
+    for(int i=0; i<strlen(str); i++)
+    {
+        if(i == 0 && str[i] == '9')
+        {
+            printf("%c", str[i]);
+            flag = 1;
+            continue;
+        }
+        int num = str[i] - '0';
+        int diff = 9 - num;
+        if(flag == 0 && (num == 0 || diff == 0))
+        {
+            continue;
+        }
+        else if(num <= diff )
+        {
+            printf("%d", num);
+            flag = 1;
+        }
+        else if(diff <= num )
+        {
+            printf("%d", diff);
+            flag = 1;
+        }
+    }
+
+}
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char str[8];
+    scanf("%s", str);
+    int i, j;
+    if(str[0] != '9')
+    {
+        j = 0;
+    }
+    else
+    {
+        j = 1;
+    }
+    for(i=j; i<strlen(str); i++)
+    {
+        if(str[i] >= '5')
+        {
+            str[i] = '9' - str[i] + '0';
+        }
+    }
+    printf("%s", str);
+}
+
+PY3:
+
+x=int(input())
+arr=[int(i) for i in str(x)]
+k=[]
+c=0
+for i in arr:
+    if c==0 and i==9:
+        k.append(i)
+        c+=1
+        continue
+    if i>=5:
+        k.append(9-i)
+    else:
+        k.append(i)
+    c+=1
+for i in k:
+    print(i,end="")
 */
