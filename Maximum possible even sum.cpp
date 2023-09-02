b97191e1095b2f3eb2f70baad41110b4c35b1156
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-02 20:26:14 +0530

    Update Maximum possible even sum.cpp

--- Maximum possible even sum.cpp
+++ Maximum possible even sum.cpp
@@ -38,9 +38,69 @@ Output: 90
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    int k, o = 0, e = 0, m = 1001;
+    
+    while(cin>>k)
+    {
+        k & 1 ? ((o += k), (m = min(k, m))) : (e += k);
+    }
+    cout<<(e + (o & 1 ? (o - m) : o));
+}
 
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int arr[N], sum = 0;
+    
+    for(int i=0; i<N; i++)
+    {
+        scanf("%d ", &arr[i]);
+        sum += arr[i];
+    }
+    
+    if(sum % 2 == 0)
+    {
+        printf("%d", sum);
+        return 0;
+    }
+    
+    int ans = 0;
+    
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i] % 2 != 0 && sum - arr[i] > ans)
+        {
+            ans = sum - arr[i];
+        }
+    }
+    printf("%d", ans);
+}
 
+PY3:
+n = int(input())
+l = sorted(list(map(int,input().strip().split())))
+if sum(l)%2:
+    s = sum(l)
+    for i in l:
+        if i%2:
+            s-=i
+        if not s%2:
+            break
+    print(s)
+else:
+    print(sum(l))
 */
