2c26315667e0c3c70c1c0f2f12f018d18c03ae35
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-02 20:05:39 +0530

    Create Divisors of N - digits in N.cpp

---
+++ Divisors of N - digits in N.cpp
@@ -0,0 +1,46 @@
+/*
+The program must accept an integer N as the input. 
+The program must find all the combinations of the digits in the integer N. 
+Then the program must print the combinations which are the factors of N in ascending order as the output. 
+If there is no such combination then the program must print -1 as the output. 
+
+Note: Exclude 1 and N from the factors of N. 
+
+Boundary Condition(s): 1 <= N <= 999999999 
+
+Input Format:  The first line contains N. 
+
+Output Format: The lines each contain an integer or the first line contains -1. 
+
+Example Input/Output 1: 
+
+Input:  12345 
+
+Output: 
+
+3 
+5 
+15 
+
+Explanation: 
+
+The integer N (12345) is divisible by 3, 5 and 15 which are all the combinations of the digits in the integer 12345. 
+3 5 15 are already in sorted order.
+So the output is 3 5 15. 
+
+Example Input/Output 2: 
+
+Input:  21
+
+Output: -1
+
+SOLTUION:
+*/
+
+
+
+/*
+
+
+
+*/
