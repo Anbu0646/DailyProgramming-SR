1da7c6d81e07f52a8baa009a0a498505e7270650
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-05 10:29:12 +0530

    Update Reverse String except vowels.cpp

--- Reverse String except vowels.cpp
+++ Reverse String except vowels.cpp
@@ -30,7 +30,39 @@ Output: Alppe
 SOLUTION:
 */
 
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int l = 0, j = 0;
+    char s[1001];
+    scanf("%s%n", s, &l);
+    char v[11] = "aeiouAEIOU";
+    char r[l + 1];
+    for(int i=l - 1; i>=0; i--)
+    {
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+        {
+            //no
+        }
+        else
+        {
+            r[j++]=s[i];
+        }
+    }
+    int k = 0;
+    for(int i=0; i<l; i++)
+    {
+        if(strchr(v, s[i]) == 0)
+        {
+            printf("%c", r[k++]);
+        }
+        else
+            printf("%c", s[i]);
+    }
+    return 0;
+}
 
 /*
 PY3:
@@ -50,4 +82,37 @@ for i in range(0, len(S)):
     else:
         print(Mod[Len], end="")
         Len -= 1
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int isv(char c)
+{
+    c=tolower(c);
+    if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    char s[1000],a[1000];
+    int i, p = 0;
+    scanf("%s",s);
+    for(i=0; i<strlen(s); i++)
+    {
+        if(!isv(s[i]))
+            a[p++] = s[i];
+    }
+    p--;
+    for(i=0; i<strlen(s); i++)
+    {
+        if(isv(s[i]))
+            printf("%c", s[i]);
+        else    
+            printf("%c", a[p--]);
+    }    
+}
 */
