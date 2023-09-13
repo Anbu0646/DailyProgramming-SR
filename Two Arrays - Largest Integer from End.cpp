93967f46cd9813481be62810679eafeecf039edb
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-13 21:15:49 +0530

    Update Two Arrays - Largest Integer from End.cpp

--- Two Arrays - Largest Integer from End.cpp
+++ Two Arrays - Largest Integer from End.cpp
@@ -50,8 +50,172 @@ Output: 77 40 30 25 99
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    int m,n;
+    cin>>m>>n;
+    int arr1[m], arr2[n];
+    for(int i=0; i<m; i++)
+    {
+        cin>>arr1[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr2[i];
+    }
+    if(m > n)
+    {
+        int dif = m - n;
+        for(int i = n - 1; i>=0; i--)
+        {
+            if(arr1[i + dif] > arr2[i])
+            {
+                cout<<arr1[i + dif]<<" ";
+            }
+            else
+            {
+                cout<<arr2[i]<<" ";
+            }
+        }
+        for(int i = m - n - 1; i>=0; i--)
+        {
+            cout<<arr1[i]<<" ";
+        }
+    }
+    else
+    {
+        int dif = n - m;
+        for(int i = m - 1; i>=0; i--)
+        {
+            if(arr2[i + dif] > arr1[i])
+            {
+                cout<<arr2[i + dif]<<" ";
+            }
+            else
+            {
+                cout<<arr1[i]<<" ";
+            }
+        }
+        for(int i = n - m - 1; i>=0; i--)
+        {
+            cout<<arr2[i]<<" ";
+        }
+    }
+    
+}
 
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int a[n], b[m];
+    for(int i = n - 1;i>=0; i--) scanf("%d", &a[i]);
+    for(int j = m - 1; j>=0; j--) scanf("%d", &b[j]);
+    
+    int l = n > m ? n : m;
+    
+    if(n >= m)
+    {
+        int i = 0;
+        while(i < n)
+        {
+            if(i < m && a[i] < b[i]) printf("%d ", b[i]);
+            else printf("%d ", a[i]);
+            i++;
+        }
+    }
+    else
+    {
+        int i = 0;
+    }
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int arr1[x], arr2[y];
+    for(int i=0; i<x; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+    for(int i=0; i<y; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+    
+    if(x > y)
+    {
+        int j = y - 1;
+        for(int i = x - 1; i>=0; i--)
+        {
+            if(j < 0)
+                break;
+            if(arr1[i] > arr2[j])
+            {
+                printf("%d ", arr1[i]);
+            }
+            else
+                printf("%d ", arr2[j]);
+            j--;
+        }
+        int v = x - y;
+        for(int i = v - 1; i>=0; i--)
+            printf("%d ", arr1[i]);
+    }
+    if(x < y)
+    {
+        int j = x - 1;
+        for(int i = y - 1; i>=0; i--)
+        {
+            if(j < 0)
+                break;
+            if(arr2[i] > arr1[j])
+                printf("%d ", arr2[i]);
+            else
+                printf("%d ", arr1[i]);
+            j--;
+            int v = y - x;
+            for(int i = v - 1; i>=0; i--)
+                printf("%d", arr2[i]);
+        }
+    }
+    if(x == y)
+    {
+        for(int i = x - 1; i>=0; i--)
+        {
+            if(arr1[i] > arr2[i])
+                printf("%d ", arr1[i]);
+            else
+                printf("%d ", arr2[i]);
+        }
+    }
+}
+
+PY3:
+m, n = input().split()
+m = int(m); n = int(n)
+a = [int(k) for k in input().split()]
+b = [int(k) for k in input().split()]
+a.reverse()
+b.reverse()
+for i in range(0, max(m, n)):
+    x=0; y=0
+    if i < len(a):
+        x = a[i]
+    if i < len(b):
+        y = b[i]
+    print(max(x, y), end = " ")
 */
