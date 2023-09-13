102bb6553049484b58d989824572186b9cfee7ce
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-13 23:20:24 +0530

    Create Odd (or) Even Triangle pattern.cpp

---
+++ Odd (or) Even Triangle pattern.cpp
@@ -0,0 +1,33 @@
+/*
+The program must accept an integer N as the input.
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 100 
+
+Input Format:  The first line contains N. 
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1:
+
+Input:  5
+
+Output: 
+
+1*3*5 
+-7*9 
+--11
+
+Example Input/Output 2: 
+
+Input:  8 
+
+Output: 
+
+2*4*6*8
+-10*12*14 
+--16*18 
+---20
+
+SOLUTION:
+*/
