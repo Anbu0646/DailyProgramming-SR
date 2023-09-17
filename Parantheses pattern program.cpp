c3cd663bc4db5d3ea5d4dfeaaa3c0bccafb27ac0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-17 15:49:09 +0530

    Create Parantheses pattern program.cpp

---
+++ Parantheses pattern program.cpp
@@ -0,0 +1,33 @@
+/*
+The program must accept a string S as the input.
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= Length of S <= 1000 
+
+Input Format:  The first line contains S. 
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  SkillRack
+
+Output: 
+
+(S)killRac(k) 
+S(k)illRa(c)k 
+Sk(i)llR(a)ck 
+Ski(l)l(R)ack
+Skil(l)Rack 
+
+Example Input/Output 2: 
+
+Input:  hide 
+
+Output: 
+
+(h)id(e)
+h(i)(d)e
+
+SOLUTION:
+*/
