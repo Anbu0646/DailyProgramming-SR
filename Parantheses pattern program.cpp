8f16a28ac135d7e29406a78c9db83ee5e36eb407
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-18 11:46:05 +0530

    Update Parantheses pattern program.cpp

--- Parantheses pattern program.cpp
+++ Parantheses pattern program.cpp
@@ -31,3 +31,86 @@ h(i)(d)e
 
 SOLUTION:
 */
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+string s;
+cin>>s;
+int i = 0, j = s.size()-1;
+
+while(i <= j)
+{
+    for(int z=0; z<s.size(); z++)
+    {
+        if(z == i)
+        {
+            cout<<"("<<s[z]<<")";
+        }
+        else if(z == j)
+        {
+            cout<<"("<<s[z]<<")";
+        }
+        else
+        {
+            cout<<s[z];
+        }
+    }
+    cout<<endl;
+    i++;
+    j--;
+}
+
+}
+
+/*
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[10001];
+    scanf("%s", s);
+    int j = strlen(s) - 1;
+    for(int i=0; i<=j; i++, j--)
+    {
+        for(int k=0; k<strlen(s); k++)
+        {
+            if(i == k || j == k)
+            {
+                printf("(%c)", s[k]);
+            }
+            else
+            {
+                printf("%c", s[k]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+PY3:
+a = input()
+k = (len(a) // 2)
+if len(a) % 2 != 0:
+   k += 1
+n = 0
+m = len(a) - 1
+for i in range(k):
+    for j in range(0, len(a)):
+        
+        if j == n or j == m:
+                
+            print("(" + a[j] + ")", end = "")
+        else:
+            print(a[j], end = "")
+    print()
+    n += 1
+    m -= 1
+*/
