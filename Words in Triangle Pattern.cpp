b17605c6db2dcb9fc70499eb00e4f314c80a1b5d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-18 11:54:41 +0530

    Create Words in Triangle Pattern.cpp

---
+++ Words in Triangle Pattern.cpp
@@ -0,0 +1,51 @@
+/*
+The program must accept a string S with spaces as the input.
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 2 <= Length of S <= 100
+
+Input Format:  The first line contains S. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  Very Useful One 
+
+Output: 
+
+V 
+Ve 
+Ver
+Very 
+U 
+Us
+Use
+Usef
+Usefu
+Useful
+O 
+On
+One
+
+Example Input/Output 2: 
+
+Input:  skill is power 
+
+Output: 
+
+s 
+sk
+ski
+skil
+skill
+i 
+is
+p 
+po
+pow
+powe 
+power
+
+SOLUTION:
+*/
