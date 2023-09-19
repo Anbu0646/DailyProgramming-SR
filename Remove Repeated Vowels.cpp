cd679525c08b5a72299a39446453252757d69665
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-19 20:24:32 +0530

    Create Remove Repeated Vowels.cpp

---
+++ Remove Repeated Vowels.cpp
@@ -0,0 +1,31 @@
+/*
+The program must accept a string S containing only lower case alphabets as the input. 
+The program must remove the repeated vowels in the string S. 
+Then the program must print the modified string S as the output.
+
+Boundary Condition(s): 1 <= Length of S <= 10^4 
+
+Input Format:  The first line contains S. 
+
+Output Format: The first line contains the modified string S. 
+
+Example Input/Output 1: 
+
+Input:  conditioner
+
+Output: cndtner 
+
+Explanation: 
+
+The vowels o and i are repeated in the string "conditioner". 
+So they are removed from the string "conditioner". 
+Hence the output is cndtner.
+
+Example Input/Output 2: 
+
+Input:  skillrack 
+
+Output: skillrack
+
+SOLUTION:
+*/
