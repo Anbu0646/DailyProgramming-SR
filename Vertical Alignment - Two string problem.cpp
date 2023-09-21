f4e91952ffc64c6a3abcbf3e53d88fc431207f3b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-21 20:40:29 +0530

    Update Vertical Alignment - Two string problem.cpp

--- Vertical Alignment - Two string problem.cpp
+++ Vertical Alignment - Two string problem.cpp
@@ -43,6 +43,47 @@ itssnot-long-text
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s1, s2, s3;
+    getline(cin, s1);
+    getline(cin, s2);
+    s1.pop_back();
+    int n = s1.size(), m = s2.size();
+
+    for(int i=0; i<max(n, m); i++)
+    {
+        if(i < n)
+        {
+            if(s1[i] == ' ' || s1[i] == '\r') 
+               s1[i]='-';
+        }
+        else s1.push_back('-');
+        if(i<m)    
+        {
+            if(s2[i] == ' ') 
+               s2[i]='-';
+        }
+        else s2.push_back('-');
+    }
+    
+    int x = max(n, m);
+
+    for(int i=0; i<x; i++)
+    {
+        if(s2[i] == '-')
+            swap(s2[i], s1[i]);
+    }
+
+    cout<<s1;
+    cout<<"\n";
+    cout<<s2;
+
+}
 
 /*
 PY3:
@@ -86,4 +127,81 @@ while i < Max:
 
 print(Res1)
 print(Res2)
+
+a,b = list(input().strip().replace(' ','-')),list(input().strip())  
+
+if len(a)>len(b): 
+    b+=[' ']*(len(a)-len(b)) 
+else:
+    a+=['-']*(len(b)-len(a)) 
+
+for i in range(len(a)): 
+    if b[i]==' ':b[i],a[i]=a[i],'-'
+
+print(''.join(a))
+print(''.join(b))
+
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void verticalalign(char *s1,char *s2,int l1,int l2)
+{
+    int min, max, i;
+    l1--;
+    if(l1 < l2)
+    {
+        min = l1;
+        max = l2;
+    }
+    else
+    {
+        min = l2;
+        max = l1;
+    }
+    if(min == l1)
+    {
+        for(i=min; i<l2; i++)
+        {
+            s1[i] = ' ';
+        }
+        s1[max] = '\0';
+    }
+    else
+    {
+        for(i=min; i<l1; i++)
+        {
+            s2[i] = ' ';
+        }
+        s2[max] = '\0';
+    }
+    for(i=0; i<max; i++)
+    {
+        if(s2[i] == ' ')
+        {
+            s2[i] = s1[i];
+            if(s1[i] == ' ')
+            {
+                s2[i] = '-';
+            }
+            s1[i] = '-';
+        }
+        if(s1[i] == ' ')
+        {
+            s1[i] = '-';
+        }
+    }
+    return;
+}
+
+int main()
+{
+    char s1[1001], s2[1001];
+    scanf("%[^\n]\n%[^\n]", s1, s2);
+    int l1 =s trlen(s1), l2 = strlen(s2);
+    verticalalign(s1, s2, l1, l2);
+    printf("%s\n%s", s1, s2);
+    return 0;
+}
 */
