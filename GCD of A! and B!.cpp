8a2190d2873c97cbcb7bf80a7eb73a1306bcd0dd
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-22 22:44:50 +0530

    Create GCD of A! and B!.cpp

---
+++ GCD of A! and B!.cpp
@@ -0,0 +1,33 @@
+/*
+The program must accept two integers A and B as the input. 
+The program must print the GCD (Greatest Common Divisor) of A! and B! as the output. 
+
+Note: A! represents the factorial of A. B! represents the factorial of B. 
+
+Boundary Condition(s): 1 <= A, B <= 20 
+
+Input Format:  The first line contains A and B separated by a space. 
+
+Output Format: The first line contains the GCD of (A!) and (B!). 
+
+Example Input/Output 1: 
+
+Input:  4 3 
+
+Output: 6
+
+Explanation:
+
+4! = 4 x 3 x 2 x 1 = 24 
+3! = 3 x 2 x 1 = 6 
+The GCD of 24 and 6 is 6. 
+So 6 is printed as the output. 
+
+Example Input/Output 2: 
+
+Input:  4 5 
+
+Output: 24
+
+SOLUTION:
+*/
