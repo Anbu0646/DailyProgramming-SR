59d255c1a572c6251a259327466a4bad43d6ba00
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-23 12:06:17 +0530

    Create Count of Holidays.cpp

---
+++ Count of Holidays.cpp
@@ -0,0 +1,32 @@
+/*
+The program must accept an integer N representing the number of days as the input. 
+The program must print the minimum and maximum possible number of holidays present in the given N days if saturday and sunday are holidays.
+
+Hint: The first day in the given N days can be any one among the 7 week days. 
+
+Boundary Condition(s): 1 <= N <= 100 
+
+Input Format:  The first line contains N. 
+
+Output Format: The first line contains the minimum and maximum possible number of holidays separated by a space.
+
+Example Input/Output 1: 
+
+Input:  10 
+
+Output: 2 4 
+
+Explanation: 
+
+The minimum possible number of holidays 2 is obtained if the 1st day starts from Monday. 
+The maximum possible number of holidays 4 is obtained if the 1st day starts from Saturday. 
+Hence the output is 2 4.
+
+Example Input/Output 2: 
+
+Input:  7
+
+Output: 2 2
+
+SOLUTION:
+*/
