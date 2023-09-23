ec139b224e24c0234ab3d68d71e8320804c05b74
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-23 12:17:40 +0530

    Update Find X and Y from their factors.cpp

--- Find X and Y from their factors.cpp
+++ Find X and Y from their factors.cpp
@@ -30,9 +30,73 @@ Output: 6 6
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)  
+{
+    vector<int> v;
+    int val;
+    map<int, int> mp;
+    
+    while(cin>>val)
+    {
+        v.push_back(val);
+        mp[val]++;
+    }
+  
+    int x = *max_element(v.begin(), v.end());
+    int ans = x;
+  
+    for(auto& it:mp)
+    {
+        int val = it.first;
+        int cnt = it.second;
+    
+        if(x % val || cnt == 2)
+         ans = val;
+    }
+    cout<<x<<" "<<ans;
+}
 
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int arr[1001], n = 0, x = 0, y = 0;
+    while((scanf("%d", &arr[n])) == 1) 
+    {
+        if(arr[n] > x)
+            x = arr[n];
+        n++;
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(x % arr[i] != 0 && arr[i] > y)
+        {
+            y = arr[i];
+        }
+    }
+    if(y == 0)
+        y = x;
+    printf("%d %d", x, y);
+}
 
+PY3:
+l = [int(k) for k in input().split()]
+l.sort()
+x = max(l)
+t = x
+k = 0
+l.remove(t)
+v = l.copy()
+for i in range (len(l) - 1, -1, -1):
+    if t != l[i] and x % l[i] == 0:
+        t = l[i]
+        v.remove(l[i])
+print(x, max(v))
 */
