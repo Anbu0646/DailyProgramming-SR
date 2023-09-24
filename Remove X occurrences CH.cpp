8824cf3cf0d496fa971c1a9ab687839088781a68
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-09-24 12:17:12 +0530

    Create Remove X occurrences CH.cpp

---
+++ Remove X occurrences CH.cpp
@@ -0,0 +1,52 @@
+/*
+The program must accept a string S, a character CH and an integer X as the input. 
+The program must remove the first X occurrences of the character CH in the string S. 
+Then the program must print the modified string S as the output. 
+If all the characters are removed from the string S, the program must print -1 as the output. 
+
+Note: The string S is case sensitive. 
+
+Boundary Condition(s): 
+
+1 <= Length of S <= 1000 
+1 <= X <= 100 
+
+Input Format:  The first line contains S. The second line contains CH and X separated by a space. 
+
+Output Format: The first line contains the modified string S or -1 as per the given condition(s). 
+
+Example Input/Output 1: 
+
+Input:  
+
+Digitization 
+i 3 
+
+Output:  Dgtzation 
+
+Explanation: 
+
+Here the string is Digitization. 
+After removing the first 3 occurrences of i, the string becomes Dgtzation. 
+Hence the output is Dgtzation. 
+
+Example Input/Output 2: 
+
+Input:
+
+B2b5b4@ABC 
+B 4
+
+Output:  2b5b4@AC 
+
+Example Input/Output 3:
+
+Input:
+
+AAAA 
+A 7
+
+Output: -1
+
+SOLUTION:
+*/
