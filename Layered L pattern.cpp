16673e5d86a8dd770ccd03189f01591973d00a99
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-01 11:18:38 +0530

    Create Layered L pattern.cpp

---
+++ Layered L pattern.cpp
@@ -0,0 +1,41 @@
+/*
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= N <= 100 
+
+Input Format:  The first line contains N. 
+
+Output Format: The first N lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  5
+
+Output: 
+
+1 
+1 2 
+1 2 3
+1 2 2 2 
+1 1 1 1 1 
+
+Example Input/Output 2: 
+
+Input:  10 
+
+Output: 
+
+1 
+1 2
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+1 2 3 4 5 5
+1 2 3 4 4 4 4
+1 2 3 3 3 3 3 3 
+1 2 2 2 2 2 2 2 2
+1 1 1 1 1 1 1 1 1 1
+
+SOLUTION:
+*/
