938a5525d9b94f1ba62f3da519cdfadcb646206a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-02 11:56:54 +0530

    Update Layered L pattern.cpp

--- Layered L pattern.cpp
+++ Layered L pattern.cpp
@@ -39,3 +39,87 @@ Output:
 
 SOLUTION:
 */
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+  
+    for(int i=1; i<=n; i++)
+    {
+        int c = 1;
+      
+        for(int j=1; j<=i; j++)
+        {
+            if(i < n/2 + 1)
+            {
+                cout<<c<<" ";
+                c++;
+            }
+            else if(i > n/2)
+            {
+               if(j <= n - i)
+               {
+                   cout<<c<<" ";
+                   c++;
+               }
+               else
+               {
+                   cout<<c<<" ";
+               }
+            }
+        }
+        cout<<endl;
+    }
+}
+
+/*
+PY3:
+n = int(input())
+lis = []
+for i in range(1, n + 1):
+    if i <= (n + 1)//2:
+        k = list(range(1, i + 1))
+        lis.append(k)
+        print(*k)
+    else:
+        j = lis[n - i]
+        print(*j + [j[-1]] * (i - len(j)))
+        
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n, k;
+    scanf("%d", &n);
+    k = (n/2);
+    if(n % 2 != 0)   
+       k = (n/2) + 1;
+    for(int i=1; i<=k; i++)
+    {
+        for(int j=1; j<=i; j++) 
+            printf("%d ", j);
+         printf("\n");
+    }
+    if(n % 2 != 0) 
+       k -= 1;
+    int v = k;
+    for(int i=n - k + 1; i<=n; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            if(j >= v) 
+               printf("%d ", v);
+            else printf("%d ", j);
+        }
+        printf("\n");
+        v -= 1;
+    }
+}   
+*/
