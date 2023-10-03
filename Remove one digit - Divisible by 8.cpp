85d62334097294b14bffd9c3df29ca6bfe62de74
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-03 22:30:42 +0530

    Update Remove one digit - Divisible by 8.cpp

--- Remove one digit - Divisible by 8.cpp
+++ Remove one digit - Divisible by 8.cpp
@@ -31,8 +31,67 @@ Output: NO
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s,out;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        out = s.substr(0, i) + s.substr(i + 1);
+        int x = stoi(out);
+        if(x != 0 && x % 8 == 0)
+        {
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+}
 
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    long long int n, d = 0;
+    scanf("%lld", &n);
+    long long int arr[1000], r[1000], j = 0, k = 0;
+    while(n > 0)
+    {
+        arr[j] = n % 10;
+        j++;
+        n = n / 10;
+    }
+    for(int i=j - 1; i>=0; i--)
+    {
+        r[k] = arr[i];
+        k++;
+    }
+    for(int i=0; i<k; i++)
+    {
+        d = 0;
+       for(int p=0; p<k; p++)
+       {
+           if(i != p)
+           {
+               d = (d * 10) + r[p];
+           }
+       }
+       if(d % 8 == 0 && d != 0)
+       {
+           printf("YES");
+           return;
+       }
+    }
+    printf("NO");
+}
+
 PY3:
 N = input().strip()
 for i in range(0, len(N)):
