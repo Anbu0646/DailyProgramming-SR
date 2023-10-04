69cfbea4fbefdf2ab2dcc16caf990273236aae6d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-04 13:00:38 +0530

    Update Largest Integer - Removing digit.cpp

--- Largest Integer - Removing digit.cpp
+++ Largest Integer - Removing digit.cpp
@@ -31,3 +31,50 @@ Output: 618
 
 SOLUTION:
 */
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    long long int n, u = 0, h = 0, l1 = 0, l2 = 0, l3 = 0, a, b, c;
+    cin>>n;
+    a = n, l1 = a/10, b = n, u = n % 10, h = n % 100;
+    l2 = (b / 100) * 10 + u, c = n, l3 = (c / 1000) * 100 + h;
+    cout<<max(l1, max(l2, l3));
+}
+
+/*
+PY3:
+n = input()
+l = []
+for i in range(len(n) - 1, len(n) - 4, -1):
+    l.append(int(n[:i] + n[i + 1:]))
+print(max(l))
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+   char str[20], tempstr[20];
+   int len, templen;
+   long num, max = 0;
+   scanf("%s%n", str, &len);
+   for(int i=len - 3; i<len; i++)
+   {
+       templen = 0;
+       for(int j=0; j<len; j++)
+       {
+           if(i != j) 
+              tempstr[templen++] = str[j];
+       }
+       tempstr[templen] = '\0';
+       num = atoi(tempstr);
+       if(num>max) max = num;
+   }
+   printf("%ld", max);
+}
+*/
