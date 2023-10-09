47aef8302f5aa7412f6db718454112d03513238e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-09 19:51:08 +0530

    Update Next prime or Not.cpp

--- Next prime or Not.cpp
+++ Next prime or Not.cpp
@@ -30,8 +30,92 @@ Output: NO
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+
+bool isPrime(int n)
+{
+    if(n == 1) 
+       return 0;
+    
+    if(n == 2 or n == 3) 
+       return 1;
+    
+    if(n % 2 == 0 or n % 3 == 0) 
+       return 0;
+    
+    for(int i = 5; i * i <= n; i += 2)
+    {        
+        if( n%i == 0 or n%(i+2) == 0) 
+            return 0;   
+    }
+    
+    return 1;
+}
+
+int main(int argc, char** argv)
+{
+    int x,y;
+    
+    cin>>x>>y;
+    
+    int k;
+    for(int i = x + 1; i <= y; i++)
+    {
+        if(isPrime(i))
+        {
+            k = i;
+            break;
+        }
+    }
+    
+    if(y == k) 
+       cout<<"YES";
+    else cout<<"NO";
+
+    return 0;    
+}
 
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    int f = 1;
+    for(int i=x; i<=y; i++)
+    {
+        int c = 0;
+        for(int j=1; j<=i; j++)
+        {
+            if(i % j == 0)
+            {
+                c++;
+            }
+        }
+        if(c == 2 && i != x && i != y)
+        {
+            f = 0;
+            break;
+        }
+    
+    }
+    if(f == 1)
+    {
+        printf("YES");
+    }
+    else
+    {    
+        printf("NO");
+    }
+    return 0;
+}
+
 PY3:
 X, Y = map(int, input().split())
 Index = 0
