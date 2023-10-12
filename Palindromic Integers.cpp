cfbe7fb8ac2833ce85ddec71404d6ea5b7a0a544
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-12 19:46:15 +0530

    Create Palindromic Integers.cpp

---
+++ Palindromic Integers.cpp
@@ -0,0 +1,30 @@
+/*
+The program must accept two integers X and Y as the input. 
+The program must print all the palindromic integers from X to Y as the output. 
+If there is no palindromic integers from X to Y then the program must print -1 as the output.
+
+Boundary Condition(s): 1 <= X, Y <= 10^5
+
+Input Format:  The first line contains X and Y separated by a space. 
+
+Output Format: The first line contains the palindromic integers from X to Y separated by a space or -1. 
+
+Example Input/Output 1:
+
+Input:  120 191 
+
+Output: 121 131 141 151 161 171 181 191 
+
+Example Input/Output 2: 
+
+Input:  455 463 
+
+Output: -1
+
+SOLUTION:
+*/
+
+
+/*
+
+*/
