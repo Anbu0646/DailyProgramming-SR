838491d89eef0fb0b133776b98c2c205518628c5
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-14 11:36:03 +0530

    Create Vertical String Pattern.cpp

---
+++ Vertical String Pattern.cpp
@@ -0,0 +1,52 @@
+/*
+The program must accept N string values as the input. 
+The program must print those N string values vertically as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= N, Length of each string <= 100 
+
+Input Format:  The first line contains N. The second line contains N string values separated by a space.
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input: 
+
+3 
+All is well
+
+Output: 
+
+Aiw 
+lse 
+l*l 
+**l 
+
+Example Input/Output 2:
+
+Input:  
+
+4 
+Robotic process automation RPA
+
+Output:
+
+RpaR
+oruP
+botA 
+oco* 
+tem* 
+isa*
+cst*
+**i* 
+**o* 
+**n*
+
+SOLUTION:
+*/
+
+
+
+/*
+
+*/
