8878be6ff81892d41d54fa9db74001b48a51cce2
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-15 13:05:27 +0530

    Create String - Asterisk Zig Zag Pattern.cpp

---
+++ String - Asterisk Zig Zag Pattern.cpp
@@ -0,0 +1,50 @@
+/*
+The program must accept a string S as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s): 2 <= Length of S <= 100 
+
+Input Format:  The first line contains S. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  abcde
+
+Output: 
+
+abcde
+abcd* 
+*bcde
+abc**
+**cde 
+ab*** 
+***de 
+a**** 
+****e 
+*****
+*****
+
+Example Input/Output 2:
+
+Input:  How 
+
+Output: 
+
+How 
+Ho* 
+*ow 
+H** 
+**w 
+***
+***
+
+SOLUTION:
+*/
+
+
+
+/*
+
+*/
