21973c43c28a7ebe7a73be0d382533e187062978
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-16 20:08:57 +0530

    Create Interlace 1 to N - Patter.cpp

---
+++ Interlace 1 to N - Patter.cpp
@@ -0,0 +1,44 @@
+/*
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 2 <= N <= 50 
+
+Input Format:  The first line contains N. 
+
+Output Format: The N lines contain the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+
+Input: 5 
+
+Output: 
+
+1 5 2 4 3 3 4 2 5 1 
+* 1 4 2 3 3 2 4 1 * 
+* * 1 3 2 2 3 1 * * 
+* * * 1 2 2 1 * * * 
+* * * * 1 1 * * * *
+
+Example Input/Output 2: 
+
+Input: 4
+
+Output: 
+
+1 4 2 3 3 2 4 1 
+* 1 3 2 2 3 1 *
+* * 1 2 2 1 * *
+* * * 1 1 * * *
+
+SOLUTION:
+*/
+
+
+
+/*
+C:
+
+PY3:
+
+*/
