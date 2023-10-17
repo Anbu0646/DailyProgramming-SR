cbfd5e7b05057971f85683e33cd22f6036016235
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-17 20:34:42 +0530

    Update Interlace 1 to N - Patter.cpp

--- Interlace 1 to N - Patter.cpp
+++ Interlace 1 to N - Patter.cpp
@@ -34,11 +34,91 @@ Output:
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    int a = n;
+  
+    for(int i=1; i<=n; i++)
+    {
+        if(i != 1)
+        {
+            for(int j=1; j<=i - 1; j++)
+                cout<<"*"<<" ";
+        }
+      
+        for(int j=1, k=a; j<=a, k>=1; j++, k--)
+            cout<<j<<" "<<k<<" ";
+        
+        a--;
+      
+        if(i != 1)
+        {
+            for(int j=1; j<=i - 1; j++)
+                cout<<"*"<<" ";
+        }
+      
+        cout<<endl;
+    }
+}
 
 /*
 C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x, y, l;
+    
+    scanf("%d", &l);
+    for(int i=0; i<l; i++)
+    {
+        x = 1;
+        y = l - i;
+        
+        for(int j=0; j<i; j++)
+        {
+            printf("* ");
+        }
+        
+        while(x <=l - i && y >= 1)
+        {
+            printf("%d ", x);
+            x++;
+            printf("%d ", y);
+            y--;
+        }
+        
+        for(int j=0; j<i; j++)
+        {
+            printf("* ");
+        }
+        
+        puts("     ");
+    }
+}
 
 PY3:
+n = int(input()) 
+a = 1 ; b = 2 ; k = n - 1
 
+def call(n):
+    l = []
+    for i in range(1, n + 1):
+        l.append(i) 
+        l.append(n - i + 1)
+    return l
+print(*call(n))
+
+for i in range(n - 1):
+    print((i + 1) * "* ", end = "")
+    print(*call(k), end = " ")
+    print((i + 1) * "* ", sep = "")
+    k -= 1
 */
