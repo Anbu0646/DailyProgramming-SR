cda85d707117b373310d07d685ac562e5b0c7c7d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-20 21:34:29 +0530

    Update Reverse Four Parts.cpp

--- Reverse Four Parts.cpp
+++ Reverse Four Parts.cpp
@@ -31,9 +31,79 @@ Output: F A S T
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin >> s;
+  
+    if(s.length() % 4 == 0)
+    {
+        int r = s.length()/4;
+        for(int i = 0; i < s.length(); i += r)
+        {
+            string c = s.substr(i, r);
+            reverse(c.begin(), c.end());
+            cout << c << " ";
+        }
+    }
+
+}
 
 
+
+
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    int n = s.length();
+    int k = n / 4;
+    for(int i=0;i<n;i+=k)  
+    {
+        for(int j = i + (k - 1); j >= i; j--)
+        {
+            cout<<s[j];
+        }
+        cout<<" ";
+    }
+}
 /*
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    char str[1001];
+    scanf("%s ", str);
+    int len = strlen(str);
+    int k = len / 4;
+    int m = 0, s = 0, l = 0;
+    for(int i=0; i<4; i++)
+    {
+       
+      m = l;
+      l = l + k;
+      for(int j=l - 1; j >= m; j--)
+      {
+        printf("%c", str[j]);  
+      }
+      printf(" ");
+    }
+}
 
+PY3:
+n = input().strip();
+t = len(n)//4;
+for i in range(0, len(n), t):
+    print(n[i:i + t][::-1], end = " ");
 */
