1ac79862d51e8000df20a5b5892ea8086e67d8d3
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-23 13:08:06 +0530

    Update Remove Consecutive Even (or) Odd Digits.cpp

--- Remove Consecutive Even (or) Odd Digits.cpp
+++ Remove Consecutive Even (or) Odd Digits.cpp
@@ -34,10 +34,68 @@ Output: 9242
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int even(int n)
+{
+    int last = n % 10;
+    while(last % 2 == 0)
+    {
+        n = n / 10;
+        last = n % 10;
+    }
+    return n;
+}
+
+int odd(int n)
+{
+    int l = n % 10;
+    
+    while(l % 2 == 1)
+    {
+        n = n / 10;
+        l = n % 10;
+    }
+    
+    return n;
+}
+
+int main(int argc, char** argv)
+{
+    int n;
+    cin>>n;
+    int jk = n % 10;
+    
+    if(jk % 2 == 0)
+    {
+        even(n);
+        cout<<even(n);
+    }
+    else
+    {
+        odd(n);
+        cout<<odd(n);
+    }
+    
+    return 0;
+}
 
 /*
 PY3:
+n = int(input());
+if n % 2 != 0:
+    while(n % 2 != 0):
+        n = n // 10;
+    print(n);
+else:
+    while(n % 2 == 0):
+        n = n // 10;
+    print(n);
+
+
+    
 N = int(input())
 Unit_Digit = N % 10
 Is_Odd = Unit_Digit % 2 == 1
@@ -51,4 +109,45 @@ while Value:
         break
     Value //= 10
 print(N//10**(Count))
+
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    long int n, i;
+    int flag = 0;
+    scanf("%llu", &n);
+    if(n % 2 == 0)
+    {
+        for(i=n; i>0; i /= 10)
+        {
+            if(i % 2 == 0)
+            {
+                flag = 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        for(i=n; i>0; i /= 10)
+        {
+            if(i % 2 != 0)
+            {
+                flag=1;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    printf("%d\n", i);
+}
 */
