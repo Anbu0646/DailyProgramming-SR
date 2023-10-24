e1cb61f5de0c5f324367de05e0149fb43f6626ee
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-24 12:01:30 +0530

    Update Consecutive 0's and 1's Binary representation.cpp

--- Consecutive 0's and 1's Binary representation.cpp
+++ Consecutive 0's and 1's Binary representation.cpp
@@ -41,7 +41,35 @@ The binary representation of 81 is 1010001.
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    long n;
+    cin>>n;
+
+    vector<int> nums;
+
+    while(n > 0)
+    {
+        nums.push_back(n % 2);
+        n /= 2;
+    }
+    
+    reverse(nums.begin(), nums.end());
+    cout<<nums[0];
+    
+    for(int i = 1; i < nums.size(); i++)
+    {
+        if(nums[i] != nums[i - 1])
+        {
+            cout<<endl;
+        }
+        cout<<nums[i];
+    }
+}
 
 /*
 PY3:
@@ -54,4 +82,43 @@ for i in range(0, len(Bin_N) - 1):
         print()
 
 print(Bin_N[len(Bin_N) - 1])
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    long int a;
+    int x[1234567], i = 0, Digit, y = 5;
+    scanf("%ld", &a);
+    
+    while(a > 0)
+    {
+        Digit = a % 2;
+        x[i] = Digit;
+        i++;
+        a = a / 2;
+    }
+    
+    for(int j = i - 1; j >= 0; j--)
+    {
+        if(y == 1)
+        {
+            if(x[j] != 1)
+            {
+                printf("\n");
+            }
+        }
+        else if(y == 0)
+        {
+            if(x[j] != 0)
+            {
+                printf("\n");
+            }
+        }
+        printf("%d", x[j]);
+        y = x[j];
+    }
+}
 */
