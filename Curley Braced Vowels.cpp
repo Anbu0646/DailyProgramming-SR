a07d69af0801bbccb4aec020685b6f783c737b07
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-27 21:10:04 +0530

    Create Curley Braced Vowels.cpp

---
+++ Curley Braced Vowels.cpp
@@ -0,0 +1,43 @@
+/*
+The program must accept a string S as the input. 
+For each substring containing only vowels in the string S, the program must enclose the substring by a pair of curly braces {}. 
+Then the program must print the modified string S as the output. 
+
+Boundary Condition(s): 1 <= Length of S <= 100
+
+Input Format:  The first line contains S.
+
+Output Format: The first line contains the modified string S. 
+
+Example Input/Output 1:
+
+Input:  bookreading 
+
+Output: b{oo}kr{ea}d{i}ng 
+
+Explanation: 
+
+Here the string is bookreading. 
+The substring containing only vowels in S are oo ea and i.
+So they are enclosed by the pair of curly braces {}. 
+Hence the output is b{oo}kr{ea}d{i}ng.
+
+Example Input/Output 2: 
+
+Input:  AeIOu 
+
+Output: {AeIOu} 
+
+Example Input/Output 3: 
+
+Input:  Apple
+
+Output: {A}ppl{e}
+
+SOLUTION:
+*/
+
+/*
+
+
+*/
