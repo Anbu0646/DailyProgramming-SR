a41731b7718f642288a89bbb7032dfd2cd49944e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-10-29 12:35:20 +0530

    Create Substring - Maximum length.cpp

---
+++ Substring - Maximum length.cpp
@@ -0,0 +1,48 @@
+/*
+The program must accept two string values S1 and S2 are of equal length as the input.
+The program must print the common substring which is having the maximum length and occurring at the same position in both string values. 
+If there is more than one such substring then print the first occurring one as the output. 
+
+Note: At least one character is always present at the same position in both string values.
+
+Boundary Condition(s): 1 <= Length of S1, S2 <= 1000
+
+Input Format:  
+
+The first line contains S1. 
+The second line contains S2.
+
+Output Format: The first line contains the common substring which is having the maximum length and occurring at the same position in both string values. 
+
+Example Input/Output 1: 
+
+Input:  
+
+skillrack 
+SkillRack 
+
+Output: kill 
+
+Explanation: 
+
+There are two substrings kill and ack occurring at the same position in both string values. 
+skillrack SkillRack Here the substring kill is having the maximum length. 
+So kill is printed as the output.
+
+Example Input/Output 2:
+
+Input: 
+
+abcxxyzmn 
+abdxyzkmn 
+
+Output: ab
+
+SOLUTION:
+*/
+
+
+
+/*
+
+*/
