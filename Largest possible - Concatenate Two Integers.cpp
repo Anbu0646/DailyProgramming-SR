344ad072507f52316a681df15977b87b3eb607d6
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-02 21:35:51 +0530

    Update Largest possible - Concatenate Two Integers.cpp

--- Largest possible - Concatenate Two Integers.cpp
+++ Largest possible - Concatenate Two Integers.cpp
@@ -34,8 +34,98 @@ Output: 52
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+
+  string s[3];
+  cin>>s[0]>>s[1]>>s[2];
+
+  long long mx = 0, x;
+
+  for(int i = 0; i < 2; i++)
+  {
+      for(int j = i + 1; j < 3; j++)
+      {
+          x = stoll(s[i] + s[j]);
+          mx = max(mx, x);
+          x = stoll(s[j] + s[i]);
+          mx = max(mx, x);
+      }
+  }
+  
+  cout<<mx;
+  
+}
 
 /*
+PY3:
+a = list(map(int,input().split()))
+a.sort(reverse = True)
+x = int(str(a[0]) + str(a[1]))
+y = int(str(a[1]) + str(a[0]))
+if x > y:
+    print(x)
+else:
+    print(y)
 
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int arr[3], temp = 0, count = 0;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
+    {
+        printf("0");
+        return 0;
+    }
+    
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(arr[j] > arr[i])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    
+    for(int m = arr[2]; m > 0; m /= 10)
+    {
+        if(m % 10 == 0)
+        {
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    if(count >= 2)
+    {
+        printf("%d%d", arr[1], arr[2]);
+    }
+    else
+    {
+        printf("%d%d", arr[2], arr[1]);
+    }
+    
+    return 0;
+    
+}
 */
