7b3527c91a3c32fda54628c2fc5144754c647914
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-04 13:12:04 +0530

    Update DECRYPT CODE.cpp

--- DECRYPT CODE.cpp
+++ DECRYPT CODE.cpp
@@ -33,7 +33,110 @@ Output: 20
 SOLUTION:
 */
 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+  
+    cin>>s;
+  
+    int n = s.size();
+
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] == '.') 
+           cout<<0;
+        else if(s[i] == '-')
+        {
+            if(s[i + 1] == '.') 
+               cout<<1;
+            else cout<<2;
+            
+            i+=1;
+        }
+    }
+  
+}
+
+
+
+
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    
+    cin>>s;
+    
+    for(int i = 0; i <= s.length() - 1; )
+    {  
+        if(s[i] == '.')
+        {
+            cout<<"0";
+            i++;
+        }
+        else if(s[i] == '-' && s[i + 1] == '.')
+        {
+            cout<<"1";
+            i += 2;
+        }
+        else
+        {
+            cout<<"2";
+            i += 2;
+        }
+    }
+
+}
 
 /*
+PY3:
+def decrypt(s):
+    d = {'.':'0','-.':'1','--':'2'}
+    de = ""
+    i = 0
+    while i < len(s):
+        for p, digit in d.items():
+            if s.startswith(p,i):
+                de += digit
+                i += len(p)
+                break
+    return de
+en = input()
+print(decrypt(en))
 
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char str[1001];
+    scanf("%s", str);
+    
+    for(int i = 0; i < strlen(str); i++)
+    {
+        if(str[i] == '.')
+        {
+            printf("0");
+        }
+        else if(str[i] == '-' && str[i+1] == '.')
+        {
+            printf("1");
+            i++;
+        }
+        else if(str[i] == '-' && str[i + 1] == '-')
+        {
+            printf("2");
+            i++;
+        }
+    }
+}
 */
