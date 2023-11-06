5ac3f4be6aa38e0acdddbe0b021129890f0edc63
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-06 21:15:35 +0530

    Update and rename Smallest possible integer - removing digits.cpp to Smallest possible integer - removing digits.c

--- Smallest possible integer - removing digits.c
+++ Smallest possible integer - removing digits.c
@@ -32,11 +32,39 @@ Output: 98787
 SOLUTION:
 */
 
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    long int a;
+    scanf("%ld",&a);
+    int b=a/10,c=((a/100)*10)+a%10;
+    int d = ( ( (a / 1000) * 10) + (a / 10) % 10 ) * 10 + (a % 10);
+    if(b < c && b < d)
+        printf("%ld", b);
+    else if(c < b && c < d)
+        printf("%ld", c);
+    else
+        printf("%ld", d);
+}
 
 /*
 PY3:
 N = input().strip()
 Value = [int(N[:i] + N[i + 1:]) for i in range(len(N) - 3, len(N))]
 print(min(Value))
+
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    int l = s.length();
+    cout<<stol(min({(s.substr(0, l - 3) + s.substr(l - 2)), (s.substr(0, l - 2) + s.substr(l - 1)), s.substr(0, l - 1)}));
+}
 */
