3f330dd7ad5aa249ad6c9f13ea6621261109800c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-12 12:44:17 +0530

    Create String X - Pattern.py

---
+++ String X - Pattern.py
@@ -0,0 +1,44 @@
+'''
+The program must accept a string S as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= Length of S <= 50
+
+Input Format:  The first line contains S.
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1:
+
+Input:  water 
+
+Output:
+
+w * * * a 
+* t * e *
+* * r * * 
+* t * e * 
+w * * * a
+
+Example Input/Output 2:
+
+Input:  OFFICE
+
+Output: 
+
+O * * * * F 
+* F * * I *
+* * C E * * 
+* * C E * *
+* F * * I * 
+O * * * * F
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
