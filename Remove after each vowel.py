84e50812d86fcb2f6240f4374bc5fa6b7553247a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-13 11:54:24 +0530

    Update Remove after each vowel.py

--- Remove after each vowel.py
+++ Remove after each vowel.py
@@ -40,10 +40,78 @@ Output: AIo123
 SOLUTION:
 '''
 
+s = input().strip()
+i = 0
+v = "aeiouAEIOU"
 
+while i < len(s):
+    if s[i] in v:
+        print(s[i], end = "")
+        i += 2
+    else:
+        print(s[i], end = "")
+        i += 1
 
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    char ch[10001];
+    scanf("%s", ch);
+    
+    for(int i = 0; ch[i] != '\0'; i++)
+    {
+        if(ch[i] == 'a' || ch[i] == 'A' || ch[i] == 'e' || ch[i] == 'E' || ch[i] == 'i' || ch[i] == 'I' || ch[i] == 'o' || ch[i] == 'O' || ch[i] == 'u' || ch[i] == 'U')
+        {
+            printf("%c", ch[i]);
+            i++;
+        }
+        else
+        {
+            printf("%c", ch[i]);
+        }
+    }
+}
 
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+bool vow(char ch) 
+{    
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+    {
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv)
+{
+    string name;
+    cin>>name;
+    
+    int i = 0;
+    
+    while (i < name.size()) 
+    {
+        if (vow(name[i])) 
+        {
+            cout << name[i] << "";
+            i += 2;
+        } 
+        else 
+        {
+            cout << name[i] << "";
+            i++;
+        }
+    }
+
+    return 0;
+    
+}
 '''
