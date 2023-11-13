55c75f13184fff522a43451d816d2eb819cf877b
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-13 12:14:55 +0530

    Create Square Brackets - String.py

---
+++ Square Brackets - String.py
@@ -0,0 +1,44 @@
+'''
+The program must accept a string S with spaces as the input. 
+The program must convert all the alphabets between each pair of square brackets [ ] to upper case alphabets. 
+Finally, the program must print the modified string S as the output. 
+
+Boundary Condition(s): 3 <= Length of S <= 1000 
+
+Input Format:  The first line contains S.
+
+Output Format: The first line contains the modified string S. 
+
+Example Input/Output 1:
+
+Input:  uhuu [ntu e]tuhnt[uheo 
+
+Output: uhuu [NTU E]tuhnt[uheo 
+
+Explanation:
+
+In the string uhuu [ntu e]tuhnt[uheo, the alphabets within the pair of square brackets are n t u e. 
+After coverting those alphabets into uppercase the string becomes uhuu [NTU E]tuhnt[uheo. 
+Hence the output is uhuu [NTU E]tuhnt[uheo. 
+
+Example Input/Output 2: 
+
+Input:  [robert [was] a good [king] 
+
+Output: [robert [WAS] a good [KING] 
+
+Example Input/Output 3: 
+
+Input:  [as[df]er] 
+
+Output: [as[DF]er]
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
