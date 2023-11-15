9d89a2ad61164ae21742aa08d812c2793e1e3b43
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-15 19:58:35 +0530

    Create K - Alphabets present or not.py

---
+++ K - Alphabets present or not.py
@@ -0,0 +1,50 @@
+'''
+The program must accept a string S and an integer K as the input. 
+The program must print YES if the first K lower case alphabets are present atleast once in the string S. 
+Else the program must print NO as the output. 
+
+Boundary Condition(s): 
+
+1 <= Length of S <= 100 
+1 <= K <= 26
+
+Input Format: 
+
+The first line contains S.
+The second line contains K. 
+
+Output Format: The first line contains either YES or NO. 
+
+Example Input/Output 1: 
+
+Input:  
+
+afehijklmnopzqrcstuvdwxgyb 
+5
+
+Output: YES 
+
+Explanation: 
+
+Here K = 5. 
+The first 5 lower case alphabets are a b c d e, which are present in the string afehijklmnopzqrcstuvdwxgyb. 
+So YES is printed. 
+
+Example Input/Output 2: 
+
+Input: 
+
+BdbAarCst 
+3
+
+Output: NO
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
