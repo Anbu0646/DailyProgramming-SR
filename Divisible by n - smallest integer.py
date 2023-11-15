5ba0676c97408b102b079d2e3dbd3c8e5d69969c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-15 20:10:50 +0530

    Create Divisible by n - smallest integer.py

---
+++ Divisible by n - smallest integer.py
@@ -0,0 +1,39 @@
+'''
+The program must accept three integers X, Y and Z as the input. 
+The program must find and print the smallest positive integer N based on the following conditions. 
+
+- N is divisible by Z. 
+- N is not equal to any of the integers from X to Y (where X < Y). 
+
+Boundary Condition(s): 1 <= X, Y, Z <= 10^8 
+
+Input Format:  The first line contains X, Y and Z separated by a space. 
+
+Output Format: The first line contains N. 
+
+Example Input/Output 1: 
+
+Input:  2 8 2
+
+Output: 10 
+
+Explanation: 
+
+10 is the smallest integer which is divisible by 2 and it is not equal to any integers from 2 to 8. 
+So 10 is printed as the output. 
+
+Example Input/Output 2: 
+
+Input:  5 10 4 
+
+Output: 4
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
