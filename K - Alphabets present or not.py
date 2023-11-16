ff86644d622a292a488e5d1d16008d557378c11d
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-16 22:36:22 +0530

    Update K - Alphabets present or not.py

--- K - Alphabets present or not.py
+++ K - Alphabets present or not.py
@@ -42,9 +42,96 @@ Output: NO
 SOLUTION:
 '''
 
+s = input()
+n = int(input())
+c = 0
+l = 'abcdefghijklmnopqrstyvwxyz'
 
+for i in range(n):
+    if l[i] in s:
+        c += 1
+if n == c:
+    print("YES")
+else:
+    print("NO")
 
 '''
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    string s;
+    getline(cin, s);
+    int sum;
+    cin>>sum;
+    int len = s.length();
+    int c = 0;
+    int freq[26] = {0};
+    
+    for(int i = 0; i < len; i++)
+    {
+        if(islower(s[i]))
+        {
+            freq[s[i] - 'a']++;
+        }    
+    }
+    
+    for(int i = 0; i < sum; i++)
+    {
+        if(freq[i] >= 1)
+        {
+            c++;
+        }
+    }
+    
+    if(c == sum)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
 
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[1001];
+    scanf("%s", s);
+    int n;
+    scanf("%d", &n);
+    int k = 0, p = 97;
+    
+    for(int i = 0; i < n; i++)  
+    {
+        for(int j = 0; j < strlen(s); j++)
+        {
+            int h = s[j];
+            if(p == h)
+            {
+                k++;
+                break;
+            }
+        }
+        
+        p++;
+        
+    }
+    
+    if(n == k)
+    {
+        printf("YES");
+    }
+    else
+    {  
+        printf("NO");
+    }
+}
 '''
