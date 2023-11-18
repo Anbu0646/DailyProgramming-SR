22194df29456ab27d87be0c3417b1e8e78dbcda0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-18 11:46:42 +0530

    Create Digits in ascending order.py

---
+++ Digits in ascending order.py
@@ -0,0 +1,38 @@
+'''
+The program must accept an integer N as the input. 
+The program must print YES if all the digits in N are in ascending order. 
+Else the program must print NO as the output. 
+
+Boundary Condition(s): 10 <= N <= 10^7 
+
+Input Format:  The first line contains N.
+
+Output Format: The first line contains either YES or NO. 
+
+Example Input/Output 1:
+
+Input:  2579 
+
+Output: YES 
+
+Explanation: 
+
+In 2579 all the digits are in ascending order. 
+So YES is printed as the output. 
+
+Example Input/Output 2: 
+
+Input : 52738
+
+Output: NO
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+
+'''
