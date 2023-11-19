d273a570aeddd7266274f16d586105157cc9106a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-19 11:22:51 +0530

    Update Digits in ascending order.py

--- Digits in ascending order.py
+++ Digits in ascending order.py
@@ -29,10 +29,65 @@ Output: NO
 SOLUTION:
 '''
 
-
+n = input()
+k = 0
+for i in range(1, len(n)):
+    if n[i - 1] > n[i]:
+        k = 1
+print("YES" if k == 0 else "NO")
 
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+   long long int n; 
+   scanf("%lld", &n);  
+   int ctr = 0; 
+   int count = 0;
+   
+   while(n > 0) 
+   {  
+       if((n % 10) >= (n % 100) / 10) 
+       { 
+           ctr++; 
+       }  
+       count++;
+       n /= 10; 
+   }  
+   
+   if(ctr == count) 
+   {
+      printf("YES");  
+   } 
+   else 
+   { 
+       printf("NO"); 
+   }
+}
 
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
+    long n;
+    cin>>n;
+    
+    while(n > 0)
+    {
+        int ud = n % 10, td = (n / 10) % 10;
+        if(ud < td)
+        {
+            cout<<"NO";
+            return 0;
+        }
+        n /= 10;
+    }
+    cout<<"YES";
+}
 '''
