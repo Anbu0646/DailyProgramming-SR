a5fae997bed8f5be5d6ccb66f4b2604e38848df9
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-19 11:19:51 +0530

    Create Alphabetically ordered rows.py

---
+++ Alphabetically ordered rows.py
@@ -0,0 +1,51 @@
+'''
+The program must accept a character matrix of size RxC containing only lower case alphabets as the input. 
+The program must print the count of rows in the matrix where the alphabets are sorted in alphabetical order as the output. 
+
+Boundary Condition(s): 2 <= R, C <= 50 
+
+Input Format:
+
+The first line contains R and C separated by a space. 
+The next R lines each contain C characters separated by a space. 
+
+Output Format: The first line contains the count of rows in the matrix where the alphabets are sorted in alphabetical order. 
+
+Example Input/Output 1: 
+
+Input: 
+
+5 4 
+a s d f h 
+i j k z x 
+y q r s t 
+u u t x k 
+
+Output: 2
+
+Explanation: 
+
+In 5x4 matrix, the alphabets in the second row and the fourth row are sorted in alphabetical order.
+Second row: h i j k 
+Fourth row: r s t u 
+Hence the output is 2.
+
+Example Input/Output 2:
+
+Input: 
+
+3 3
+i j o 
+a z b 
+p c e 
+
+Output: 1
+
+SOLUTION:
+'''
+
+
+
+'''
+
+'''
