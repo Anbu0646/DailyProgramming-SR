26378059fb16bc38555d95e637aeb305a900dda7
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-20 20:07:32 +0530

    Update Alphabetically ordered rows.py

--- Alphabetically ordered rows.py
+++ Alphabetically ordered rows.py
@@ -44,8 +44,93 @@ Output: 1
 SOLUTION:
 '''
 
+r, c = map(int, input().strip().split())
+mat = []
+for i in range(r):
+    li = list(map(str, input().strip().split()))
+    mat.append(li)
+count = r
+for i in range(r):
+    for j in range(c - 1):
+        if mat[i][j] > mat[i][j + 1]:
+            count -= 1
+            break
+print(count)
 
+x, y = map(int, input().split())
+a = [input().split() for i in range(x)]
+print(len([i for i in a if i == sorted(i)]))
 
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int sort(char c[], int b)
+{
+    int f = 0;
+    for(int i = 0; i < b - 1; i++)
+    {
+        for(int j = 0; j <b - i - 1; j++)
+        {
+            if(c[j] > c[j + 1])
+            {
+                f++;
+               return f;
+            }
+        }
+    }
+    
+    return f;
+    
+}
+
+int main()
+{
+    int a, b;
+    int f = 0;
+    scanf("%d %d\n", &a, &b);
+    for(int i = 0; i < a; i++)
+    {
+        char c[b];
+        for(int j = 0; j < b; j++)
+            scanf("%c ", &c[j]);
+        if(sort(c, b) == 0)    
+            f++;
+    }
+    printf("%d ", f);
+}
+
+
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int r, c;
+    cin>>r>>c;
+    char k, p;
+    int count = 0, flag;
+    for(int i = 0; i < r; i++)
+    {
+        cin>>p;
+        flag = 1;
+        for(int j = 1; j < c; j++)
+        {
+            cin>>k;
+            if(k < p)
+            {
+                flag = 0;
+            }
+            p = k;
+        }
+        if(flag == 1)
+        {
+            count++;
+        }
+    }
+    cout<<count;
+}
 '''
