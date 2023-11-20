879dcb8381202024eb82fe42e2eb0bd9e0c98801
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-20 20:12:39 +0530

    Create Asterisk Intermost & Outermost layers in Matrix.py

---
+++ Asterisk Intermost & Outermost layers in Matrix.py
@@ -0,0 +1,103 @@
+'''
+The program must accept a character matrix of size NxN containing only alphabets as the input.
+The program must replace the innermost layer and the outermost layer of the matrix by the asterisk. 
+Then the program must print the modified matrix as the output.
+
+Boundary Condition(s): 3 <= N <= 100
+
+Input Format:  
+
+The first line contains N.
+The next N lines each contain N alphabets separated by a space. 
+
+Output Format: The first N lines containing the modified matrix. 
+
+Example Input/Output 1:
+
+Input: 
+
+5
+d z y w y 
+y k h a j
+z y b w l 
+w i n t w
+e r t y u 
+
+Output: 
+
+* * * * * 
+* k h a *
+* y * w *
+* i n t * 
+* * * * * 
+
+Explanation: 
+
+In the given 5x5 matrix, the innermost layer and the outermost layer are highlighted below. 
+
+d z y w y 
+y k h a j 
+z y b w l
+w i n t w 
+e r t y u 
+
+After replacing the innermost layer and the outermost layer of the matrix by the asterisk, it becomes 
+
+* * * * *
+* k h a * 
+* y * w *
+* i n t * 
+* * * * * 
+
+Example Input/Output 2:
+
+Input: 
+
+8 
+U A L K Y A N V
+S I U G D T A Z 
+D T B J P D C A
+U Z S T Q H O C 
+A Q C U R M M C
+D R A T B O T J 
+X X S R D T P M 
+F Z F E O C G A 
+
+Output:
+
+* * * * * * * * 
+* I U G D T A * 
+* T B J P D C *
+* Z S * * H O * 
+* Q C * * M M * 
+* R A T B O T * 
+* X S R D T P * 
+* * * * * * * *
+
+Example Input/Output 3: 
+
+Input: 
+
+4 
+h x b l 
+u q i v 
+y c y o 
+e m a c 
+
+Output: 
+
+* * * * 
+* * * * 
+* * * * 
+* * * *
+
+SOLUTION:
+'''
+
+
+
+
+'''
+
+
+'''
