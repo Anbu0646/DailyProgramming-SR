bf9947ae9a42aeaade7e5cba0226fbf87758d6fd
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-21 20:20:01 +0530

    Update Asterisk Intermost & Outermost layers in Matrix.py

--- Asterisk Intermost & Outermost layers in Matrix.py
+++ Asterisk Intermost & Outermost layers in Matrix.py
@@ -94,10 +94,98 @@ Output:
 SOLUTION:
 '''
 
+n = int(input())
+a = [list(map(str, input().split())) for i in range(n)]
+k = n // 2
 
-
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print('*', end = " ")
+        elif i == k and j == k:
+            print('*', end = " ")
+        elif n % 2 == 0 and ((i == k - 1 and j == k) or (i == k and j == k - 1) or (j == k - 1 and i == k - 1)):
+            print('*', end = " ")
+        else:
+            print(a[i][j], end = " ")
+    print()
 
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d\n", &n);
+    char mat[101][101];
+    int num = ceil(n / 2.0);
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            scanf("%c ", &mat[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            if(j == 0 || j == n - 1 || i == 0 || i == n - 1)
+            {
+                printf("* ");
+            }
+            else if(n % 2 != 0)
+            {
+                if(i == num - 1 && j == num - 1)
+                {
+                    printf("* ");
+                }
+                else
+                {
+                    printf("%c ", mat[i][j]);
+                }
+            }
+            else if(n % 2 == 0)
+            {
+                if(i == num - 1 && j  == num - 1 || i == num - 1 && j == num || i == num && j == num - 1 || i == num && j == num)
+                {
+                    printf("* ");
+                }
+                else
+                {
+                    printf("%c ", mat[i][j]);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
 
 
+CPP:
+#include <iostream> 
+
+int main()
+{
+    int n;
+    std::cin>>n; 
+    
+    char mat[n][n]; 
+    
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cin>>mat[i][j];
+            if(i == 0 || j == 0 || i == n - 1 || j == n - 1 || (i == n / 2 && j == n / 2) || (n%2==0 && ( (i==n/2-1 && j==n/2-1) || (i==n/2-1 && j==n/2) || (i==n/2 && j==n/2-1) ) )) 
+               mat[i][j]='*';
+            std::cout<<mat[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
 '''
