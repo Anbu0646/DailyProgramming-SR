43e2f434b2f619f8b84f918dc467bcd05013643e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-22 12:20:28 +0530

    Update Replace Consecutive Digits by max.py

--- Replace Consecutive Digits by max.py
+++ Replace Consecutive Digits by max.py
@@ -31,8 +31,124 @@ Output: 6A
 SOLUTION:
 '''
 
-
+a, l = input(), []
+for i in a:
+    if i.isalpha():
+        if len(l) != 0:
+            print(max(l), end = '')
+            l = []
+        print(i, end = '')
+    else:
+        l.append(int(i))
+if len(l) != 0:
+    print(max(l))
+  
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[101];
+    int l;
+    scanf("%s%n", s, &l);
+    for(int i = 0; i < l; i++)
+    {
+        if(isdigit(s[i]))
+        {
+            int m = s[i] - 48;
+            for(int j=  i + 1; j <= l; j++)
+            {
+                if((!isdigit(s[j])) || j == l)
+                {
+                    i = j - 1;
+                    break;
+                }
+                int k = s[j] - 48;
+                if(m < k) 
+                   m=k;
+            }
+            printf("%d", m);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+}
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char a[101];
+    scanf("%s", a);
+    int n = strlen(a);
+    int flag = 0, max = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(flag == 0 && isalpha(a[i]))
+        {
+            printf("%c", a[i]);
+        }
+        else if(flag == 1 && isalpha(a[i]))
+        {
+            printf("%c%c", max, a[i]);
+            max = 0;
+            flag = 0;
+        }
+        else if(isdigit(a[i]))
+        {
+            flag = 1;
+            if(a[i] > max)
+            {
+                max = a[i];
+            }
+        }
+    }
+    if(flag == 1)
+    {
+        printf("%c", max);
+    }
+}
 
 
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  string str;
+  cin>>str;
+  int ind = 0;
+  while(ind < str.length())
+  {
+      if(isalpha(str[ind]))
+      {
+          cout<<str[ind];
+      }
+      else
+      {
+          int maxi = -1;
+      
+          while(isdigit(str[ind]) and ind < str.length())
+          {
+              maxi = max(maxi, str[ind] - '0');
+              ++ind;
+          }
+          
+          cout<<maxi;
+          
+          if(ind < str.length()) 
+          {
+              cout<<str[ind];
+          }
+       }
+      ind++;
+    }
+}
 '''
