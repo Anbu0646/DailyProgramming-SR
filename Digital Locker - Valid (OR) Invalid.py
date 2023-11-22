829950c5616513f1563198c00b7cab4c6af2e347
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-22 12:08:20 +0530

    Create Digital Locker - Valid (OR) Invalid.py

---
+++ Digital Locker - Valid (OR) Invalid.py
@@ -0,0 +1,49 @@
+'''
+The program must accept two string values U and P as the input. 
+U represents the username of a digital locker and P represents its password. 
+The program must print VALID if the password does not contain any characters from the username U.
+Else the program must print INVALID as the output. 
+
+Boundary Condition(s): 
+
+5 <= Length of U <= 50 
+4 <= Length of P <= 20
+
+Input Format: 
+
+The first line contains U. 
+The second line contains P. 
+
+Output Format: The first line contains either VALID or INVALID. 
+
+Example Input/Output 1:
+
+Input: 
+
+uit047@demo 
+srack123 
+
+Output: VALID
+
+Explanation:
+
+Here the password srack123 does not contains any characters from the username uit047@demo.
+So VALID is printed as the output. 
+
+Example Input/Output 2:
+
+Input: 
+
+AVNG007_bk47 
+o7st0ct2o19
+
+Output: INVALID
+
+SOLUTION:
+'''
+
+
+
+'''
+
+'''
