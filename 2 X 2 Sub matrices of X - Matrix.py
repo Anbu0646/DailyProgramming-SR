e3c35e354911a1954ebaeed91302c10a891982c8
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-22 12:12:14 +0530

    Create 2 X 2 Sub matrices of X - Matrix.py

---
+++ 2 X 2 Sub matrices of X - Matrix.py
@@ -0,0 +1,79 @@
+'''
+The program must accept an integer matrix of size RxC and an integer X (where X has occurred at least once in the matrix) as the input.
+The program must print all submatrices of size 2x2, containing the integer X at least once as shown in the Example Input/Output section. 
+
+Boundary Condition(s):
+
+2 <= R, C <= 50
+1 <= Matrix element value, X <= 1000
+
+Input Format: 
+
+The first line contains R and C separated by a space. 
+The next R lines each contain C integers separated by a space. 
+The (R+2)nd line contains X. 
+
+Output Format: The lines contain the list of 2x2 submatrices containing the integer X as shown in the Example Input/Output section. 
+
+Example Input/Output 1:
+
+Input:
+
+4 4 
+10 20 30 40
+45 78 56 89
+12 56 23 10 
+48 98 26 55
+56 
+
+Output:
+
+20 30 
+78 56 
+30 40
+56 89 
+45 78 
+12 56 
+78 56
+56 23 
+56 89 
+23 10 
+12 56 
+48 98 
+56 23
+98 26
+
+Explanation:
+
+Here X = 56. 
+There are seven 2x2 submatrices containing the integer 56.
+So they are printed as the output. 
+
+Example Input/Output 2:
+
+Input: 
+
+5 3 
+5 8 6
+8 8 2 
+2 9 9 
+8 3 5
+6 5 7 
+6 
+
+Output: 
+
+8 6 
+8 2 
+8 3 
+6 5
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
