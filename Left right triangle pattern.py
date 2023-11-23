025260c27d74e962145c3de795f3e5e8b626c2a8
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-23 21:14:05 +0530

    Create Left right triangle pattern.py

---
+++ Left right triangle pattern.py
@@ -0,0 +1,42 @@
+'''
+The program must accept a string S as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= Length of S <= 1000
+
+Input Format:  The first line contains S. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  skillrack 
+
+Output: skillrack 
+        *killrac* 
+        **illra**
+        ***llr***
+        ****l**** 
+        ***llr*** 
+        **illra**
+        *killrac* 
+        skillrack 
+        
+Example Input/Output 2: 
+
+Input:  logics 
+
+Output: logics 
+        *ogic* 
+        **gi**
+        *ogic* 
+        logics
+
+SOLUTION:
+'''
+
+
+'''
+
+
+'''
