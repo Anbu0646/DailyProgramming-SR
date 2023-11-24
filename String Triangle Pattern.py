d671684330b93d7eba50c5d5bad75e17dc9db29f
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-24 21:00:11 +0530

    Create String Triangle Pattern.py

---
+++ String Triangle Pattern.py
@@ -0,0 +1,48 @@
+'''
+The program must accept a string S as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Note: The length of S is always odd. 
+
+Boundary Condition(s): 5 <= Length of S <= 99
+
+Input Format:  The first line contains S.
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  12345 
+
+Output: **1**
+        *2*2*
+        34543 
+        
+Example Input/Output 2: 
+
+Input:  abcdefg 
+
+Output: ***a*** 
+        **b*b** 
+        *c***c*         
+        defgfed 
+        
+Example Input/Output 3:
+
+Input:  skillrack 
+
+Output: ****s**** 
+        ***k*k*** 
+        **i***i** 
+        *l*****l* 
+        lrackcarl
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
