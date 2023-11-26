1845694e1bc4740c95e06526c63c23496a12c1a0
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-26 12:50:05 +0530

    Create Opposite Diagonals Pattern.py

---
+++ Opposite Diagonals Pattern.py
@@ -0,0 +1,75 @@
+'''
+The program must accept an integer matrix of size NxN as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 2 <= N <= 50
+
+Input Format:  
+
+The first line contains N.
+The next N lines each contain N integers separated by a space.
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+
+Input: 
+
+3
+1 2 3 
+4 5 6
+7 8 9 
+
+Output: 
+
+1
+2 4 
+3 5 7
+6 8 
+9
+
+Example Input/Output 2: 
+
+Input : 
+
+2 
+1 2
+3 4
+
+Output:
+
+1 
+2 3 
+4 
+
+Example Input/Output 3:
+
+Input : 
+
+5 
+65 72 68 52 24
+57 83 61 77 86 
+41 19 74 44 32 
+86 20 69 40 63 
+27 97 46 82 31 
+
+Output: 
+
+65
+72 57 
+68 83 41 
+52 61 19 86 
+24 77 74 20 27 
+86 44 69 97
+32 40 46
+63 82
+31
+
+SOLUTION:
+'''
+
+
+'''
+
+
+'''
