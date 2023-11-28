3477b4434b8545f585908a6809209f9ef61da936
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-28 20:36:34 +0530

    Create Matrix - First Occuring Vowel.py

---
+++ Matrix - First Occuring Vowel.py
@@ -0,0 +1,62 @@
+'''
+The program must accept a character matrix of size RxC as the input. 
+The program must print the first occurring vowels from the top in each column of the matrix as the output.
+
+Note: At least one vowel is always occurred in each column of the matrix. 
+
+Boundary Condition(s): 2 <= R, C <= 100 
+
+Input Format: 
+
+The first line contains R and C separated by a space. 
+The next R lines, each contains C characters separated by a space. 
+
+Output Format: The first line contains the first occurring vowels from the top in each column of the matrix. 
+
+Example Input/Output 1: 
+
+Input:
+
+4 4 
+H N L A 
+y S I O 
+h a O # 
+e F @ a 
+
+Output: eaIA 
+
+Explanation: 
+
+The first occurring vowels from the top in each column of the matrix are highlighted below. 
+
+H N L A 
+y S I O 
+h a O # 
+e F @ a 
+
+So eaIA is printed as the output.
+
+Example Input/Output 2: 
+
+Input: 
+
+3 7 
+o f w
+$ w E 
+d t v
+M i O 
+i 1 2
+E e @ 
+R O u 
+
+Output: oEeiOEu
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
