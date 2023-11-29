262d22ed8c8b9e267b7833e1e8a00c9bc0846c52
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-29 21:19:02 +0530

    Create Integer Diamond - Pattern.py

---
+++ Integer Diamond - Pattern.py
@@ -0,0 +1,43 @@
+'''
+The program must accept an integer N as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 3 <= N <= 50 
+
+Input Format:  The first line contains N. 
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1:
+
+Input:  4 
+Output: * * * 1 * * *
+        * * 2 1 2 * * 
+        * 3 2 1 2 3 * 
+        4 3 2 1 2 3 4 
+        * 3 2 1 2 3 * 
+        * * 2 1 2 * *
+        * * * 1 * * *
+
+Example Input/Output 2:
+
+Input:  5 
+
+Output: * * * * 1 * * * *
+        * * * 2 1 2 * * *
+        * * 3 2 1 2 3 * * 
+        * 4 3 2 1 2 3 4 * 
+        5 4 3 2 1 2 3 4 5 
+        * 4 3 2 1 2 3 4 *
+        * * 3 2 1 2 3 * *
+        * * * 2 1 2 * * * 
+        * * * * 1 * * * *
+
+SOLUTION:
+'''
+
+
+'''
+
+
+'''
