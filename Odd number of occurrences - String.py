510c99b07a7bfec54ce8a0274b8849530ff42d25
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-11-30 20:07:38 +0530

    Create Odd number of occurrences - String.py

---
+++ Odd number of occurrences - String.py
@@ -0,0 +1,43 @@
+'''
+The program must accept a string S as the input. 
+The program must print the characters with an odd number of occurrences in the order of their occurrence in S as the output. 
+If there is no such character in the string S then the program must print -1 as the output. 
+
+Boundary Condition(s): 2 <= Length of S <= 1000 
+
+Input Format:  The first line contains S. 
+
+Output Format: The first line contains the characters with an odd number of occurrences in the order of their occurrence in S.
+
+Example Input/Output 1:
+
+Input:  nneettwwoorkwo 
+
+Output: work
+
+Explanation: 
+
+In the string nneettwwoorkwo, only the four characters w, o, r, and k have occurred the odd number of times. 
+Hence the output is work.
+
+Example Input/Output 2: 
+
+Input:  roboticsrobotics
+
+Output: -1 
+
+Example Input/Output 3: 
+
+Input:  5#ATE5#5# 
+
+Output: 5#ATE
+
+SOLUTION:
+'''
+
+
+
+'''
+
+
+'''
