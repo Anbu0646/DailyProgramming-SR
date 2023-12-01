5e41eb37284a477ec07e3f2f6a04f7f757b2ffb7
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-01 21:09:45 +0530

    Update Odd number of occurrences - String.py

--- Odd number of occurrences - String.py
+++ Odd number of occurrences - String.py
@@ -35,9 +35,64 @@ Output: 5#ATE
 SOLUTION:
 '''
 
-
+s = input().strip()
+res = ""
+for i in s:
+    if s.count(i) % 2 == 1 and i not in res:
+        res += i
+if len(res) == 0:
+    print(-1)
+else:
+    print(res)
 
 '''
+C:
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    char s[10001];
+    int c = 0, br[256] = {0};
+    scanf("%s", s);
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        br[s[i]]++;
+    }
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if(br[s[i]] % 2 != 0)
+        {
+            printf("%c", s[i]);
+            c = 1;
+            br[s[i]] = 0;
+          }
+    }
+    
+    if(c == 0) printf("-1");
+}
 
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    string s, k = "";
+    cin>>s;
+    for(int i = 0; i < s.length(); i++)
+    {
+        int c = count(s.begin(), s.end(), s[i]);
+        if(c % 2) 
+        {
+            if(k.find(s[i]) == string::npos)
+            k += s[i];
+    }
+}
+
+if(k.length() > 0)
+    cout<<k;
+else cout<<-1;
+}
 '''
