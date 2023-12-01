e1af13841962cf60ddef49cfcf447f878e6f06b1
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-01 21:05:14 +0530

    Create Remove - K bits.py

---
+++ Remove - K bits.py
@@ -0,0 +1,45 @@
+'''
+The program must accept two integers N and K as the input. 
+The program must remove the first K zeros in the binary representation of N. 
+If the number of zeros is less than K, the program must remove all the zeros in the binary representation of N. 
+Then the program must print the decimal equivalent of the modified binary representation of N as the output.
+
+Boundary Condition(s): 
+
+1 <= N <= 10^5 
+1 <= K <= 10 
+
+Input Format: 
+
+The first line contains N.
+The second line contains K. 
+
+Output Format: The first line contains the decimal equivalent of the modified binary representation of N. 
+
+Example Input/Output 1: 
+
+Input:  5 1 
+
+Output: 3 
+
+Explanation:
+
+The binary representation of 5 is 101. 
+After removing the first zero from 101, it becomes 11. 
+The decimal equivalent of 11 is 3. 
+Hence the output is 3.
+
+Example Input/Output 2:
+
+Input :  8 5 
+
+Output: 1
+
+SOLUTION:
+'''
+
+
+'''
+
+
+'''
