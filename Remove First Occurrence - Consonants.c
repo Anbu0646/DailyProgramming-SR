f76772884109017c7d99efcb382cbba7b57f8b0c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-02 14:59:39 +0530

    Create Remove First Occurrence - Consonants.c

---
+++ Remove First Occurrence - Consonants.c
@@ -0,0 +1,43 @@
+/*
+The program must accept a string S as the input. 
+The program must remove the first occurrence of all the repeated consonants in the string S. 
+Then the program must print the modified string S as the output. 
+
+Boundary Condition(s): 3 <= Length of S <= 1000 
+
+Input Format:  The first line contains S. 
+
+Output Format: The first line contains the modified string S. 
+
+Example Input/Output 1: 
+
+Input:  Attendance 
+
+Output: Atedance 
+
+Explanation: 
+
+The repeated consonants in the string Attendance are t and n.
+So the first occurrence of both the consonants are removed from the string Attendance. 
+Hence the output is Atedance. 
+
+Example Input/Output 2: 
+
+Input:  Cloud 
+
+Output: Cloud
+
+Example Input/Output 3: 
+
+Input:  OCcurrence@123 
+
+Output: OCurence@123
+
+SOLUTION:
+*/
+
+
+/*
+
+
+*/
