5d383d5ee8295375addccf3d82850cb1d047de78
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-03 20:30:10 +0530

    Update Remove First Occurrence - Consonants.c

--- Remove First Occurrence - Consonants.c
+++ Remove First Occurrence - Consonants.c
@@ -36,8 +36,122 @@ Output: OCurence@123
 SOLUTION:
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int iscon(char t)
+{
+    char k = toupper(t);
+    if(k == 'A' || k == 'E' || k == 'I' || k == 'O' || k == 'U')
+       return 1;
+    else
+       return 0;
+}
+
+int cou(char t, char s[])
+{
+    int z = 0;
+    for(int i = 0; i < strlen(s); i++)
+    {
+      if(t == s[i])
+         z++;
+    }
+    return z;
+}
+
+int main()
+{
+    char s[1001];
+    scanf("%s", s);
+    int a[128] = {0}, i;
+    for(i = 0; i < strlen(s); i++)
+    {
+        if(iscon(s[i]) == 0)
+        {
+            int c = cou(s[i], s);
+            if(c > 1)  
+            {
+                if(a[s[i]] == 0)
+                {
+                    a[s[i]] = 1;
+                }
+                else
+                  printf("%c", s[i]);
+            }
+            else if(c == 1)
+                 printf("%c", s[i]);
+        }
+        else
+           printf("%c", s[i]);
+    }
+  
+return 0;
+
+}
 
 /*
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int check(char ch)
+{
+    char s = tolower(ch);
+    if(s != 'a' && s != 'e' && s != 'i' && s != 'o' && s != 'u')
+    {
+        return 1;
+    }
+    return 0;
+}
 
+int main(int argc, char** argv)
+{
+    string s;
+    cin>>s;
+    int n = s.length();
+    unordered_map<char, int> mp;
+    for(int i = 0; i < n; i++)
+    {
+        if(check(s[i]))
+        {
+            mp[s[i]]++;
+        }  
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(!check(s[i]))
+        {
+            cout<<s[i];
+        }
+        else
+        {
+            int k = mp[s[i]];
+            if(k > 1)
+            {
+                mp[s[i]] = 1;
+                continue;
+            }
+            else
+            {
+                cout<<s[i];
+            }
+        }
+    }
+}
+
+PY3:
+def isvowel(c):
+    c = c.lower()
+    return c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'
+    
+s = list(map(str, input().strip())) 
+l = [] 
+
+for i in s:
+    if i not in l and (not isvowel(i)) and (s.count(i) > 1):
+        l.append(i) 
+    else:
+        print(i, end = "")
 */
