338551272bef749abaee775d67bc9a086b1d5675
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-03 20:20:53 +0530

    Update Integers with same digits.c

--- Integers with same digits.c
+++ Integers with same digits.c
@@ -43,9 +43,97 @@ Output: -1
 SOLUTION:
 */
 
+#include<stdio.h>
+#include<stdlib.h>
 
+int samedigit(int num)
+{
+    int last = num % 10;
+    while(num > 0)
+    {
+        int d = num % 10;
+        if(d != last)
+        {
+            return 0;
+        }
+        num /= 10;
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+  
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+  
+    int f = 0;
+  
+    for(int i = 0; i < n; i++)
+    {
+        if(samedigit(arr[i]))
+        {
+            printf("%d ", arr[i]);
+            f = 1;
+        }
+    }
+  
+    if(f == 0)
+       printf("-1");
+}
 
 /*
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int n, x;
+    cin>>n;
+    int fl = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>x;
+        int t = x, f = 0;
+        while(t != 0)
+        {
+            int c = t % 10;
+            if(c != (x % 10))
+            {
+                f = 1;
+                break;
+            }
+            t /= 10;
+        }
+        if(f == 0)
+        {
+            fl = 1;
+            cout<<x<<" ";
+        }
+    }
+    if(fl == 0)
+    {
+        cout<<"-1";
+    }
+}
 
 
+
+PY3:
+n = int(input())
+l = list(map(str, input().split()))
+f = 0
+for i in l:
+    if len(list(set(i))) == 1:
+        print(i, end = " ")
+        f = 1
+if f == 0:
+    print("-1")
 */
