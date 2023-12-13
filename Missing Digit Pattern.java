580d47173537b29a6121cbaf10e2382aadf4606c
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-13 18:23:03 +0530

    Create Missing Digit Pattern.java

---
+++ Missing Digit Pattern.java
@@ -0,0 +1,36 @@
+/*
+The program must accept an integer N as the input.
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Note: All the digits in N are non-zero digits. 
+
+Boundary Condition(s): 11 <= N < 10^4
+
+Input Format:  The first line contains N.
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section. 
+
+Example Input/Output 1: 
+
+Input:  651
+
+Output: 51 51 51 51 51 51
+        61 61 61 61 61 
+        65 
+
+Example Input/Output 2: 
+
+Input:  9845 
+
+Output: 845 845 845 845 845 845 845 845 845 
+        945 945 945 945 945 945 945 945 
+        985 985 985 985 
+        984 984 984 984 984
+
+SOLUTION:
+*/
+
+
+/*
+
+*/
