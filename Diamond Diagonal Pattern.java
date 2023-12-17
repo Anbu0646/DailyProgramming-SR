f68b773daf58d88523d25a7cb65d639b1b4e7745
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-17 18:22:04 +0530

    Create Diamond Diagonal Pattern.java

---
+++ Diamond Diagonal Pattern.java
@@ -0,0 +1,49 @@
+/*
+The program must accept an integer N and a character CH as the input.
+The program must print the desired pattern as shown in the Example Input/Output section.
+
+Boundary Condition(s): 4 <= N <= 50 
+
+Input Format:  The first line contains N and CH separated by a space. 
+
+Output Format: The lines contain the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1: 
+
+Input:  5 *
+
+Output: ----*
+        ---***
+        --*-*-* 
+        -*--*--*
+        *********
+        -*--*--* 
+        --*-*-*
+        ---*** 
+        ----* 
+
+Example Input/Output 2:
+
+Input:  6 # 
+
+Output: -----#
+        ----###
+        ---#-#-#
+        --#--#--#
+        -#---#---# 
+        ########### 
+        -#---#---# 
+        --#--#--# 
+        ---#-#-#
+        ----###
+        -----#
+
+SOLUTION:
+*/
+
+
+/*
+
+
+
+*/
