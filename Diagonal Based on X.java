460e8186e5402138066f870b2ba0930b44ac3651
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-18 10:29:18 +0530

    Create Diagonal Based on X.java

---
+++ Diagonal Based on X.java
@@ -0,0 +1,70 @@
+/*
+The program must accept an integer matrix of size NxN and an integer X as the input. 
+The program must print the output based on the following conditions. 
+
+- If X is odd, the program must print all the integers along the main diagonal of X. 
+- If X is even, the program must print all the integers along the opposite diagonal of X. 
+
+Note: The integer X has occurred only once in the given NxN matrix. 
+
+Boundary Condition(s): 2 <= N <= 50
+
+Input Format:  The first line contains N. 
+               The next N lines each contain N integers separated by a space. 
+               The (N+2)nd line contains X. 
+
+Output Format: The first line contains all the integers along the main diagonal or the opposite diagonal of X.
+
+Example Input/Output 1: 
+
+Input:
+
+4 
+77 48 51 82 
+13 53 76 68
+64 71 45 53
+87 87 64 24 
+45 
+
+Output: 77 53 45 24 
+
+Explanation: 
+
+The main diagonal of 45 is highlighted below. 
+
+77 48 51 82
+13 53 76 68 
+64 71 45 53 
+87 87 64 24 
+
+Example Input/Output 2: 
+
+Input:  
+
+5
+41 70 37 49 92
+84 54 46 58 81 
+36 89 53 63 70 
+22 13 20 13 59
+35 43 32 28 83 
+20 
+
+Output: 81 63 20 43 
+
+Explanation: 
+
+The opposite diagonal of 20 is highlighted below.
+
+41 70 37 49 92 
+84 54 46 58 81
+36 89 53 63 70 
+22 13 20 13 59 
+35 43 32 28 83
+
+SOLUTION:
+*/
+
+/*
+
+
+*/
