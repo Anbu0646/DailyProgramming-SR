70fb7ad5e0c0ef18d0c5ff255180f0a1d08ce992
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-20 14:14:44 +0530

    Create Rotate Even Digits - clockwise.java

---
+++ Rotate Even Digits - clockwise.java
@@ -0,0 +1,42 @@
+/*
+The program must accept an integer N as the input. 
+The program must rotate the even digits of N in their positions by 1 time in the clockwise direction. 
+Then the program must print the modified integer N as the output. 
+
+Boundary Condition(s): 100 <= N <= 10^18
+
+Input Format:  The first line contains N. 
+
+Output Format: The first line contains the modified integer N.
+
+Example Input/Output 1:
+
+Input:  5233487 
+
+Output: 5433827 
+
+Example Input/Output 2: 
+
+Input:  1002 
+
+Output: 1020 
+
+Example Input/Output 3: 
+
+Input:  400352
+
+Output: 2354 
+
+Explanation: 
+
+After rotating the even digits of 400352 in their positions by 1 time in the clockwise direction, the integer becomes 002354.
+Now the leading zeros in 002354 have been removed and it is printed as output.
+
+SOLUTION:
+*/
+
+
+/*
+
+
+*/
