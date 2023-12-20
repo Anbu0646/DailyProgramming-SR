61bbd55237587ecf4f58bfeecc47134ce63a898a
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-20 14:09:37 +0530

    Create Palindromic String.java

---
+++ Palindromic String.java
@@ -0,0 +1,33 @@
+/*
+The program must accept a string S containing only lower case alphabets as the input.
+The program must print all the palindromic substring values of length 3 in the string S as the output. 
+If there is no such palindromic substring, the program must print -1 as the output. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format:  The first line contains S.
+
+Output Format: The lines contain the palindromic substring values of length 3 or the first line contains -1. 
+
+Example Input/Output 1:
+
+Input:  eyelens 
+
+Output: eye ele 
+
+Explanation: Two palindromic substring values of length 3 in the string eyelens are eye and ele. 
+
+Example Input/Output 2:
+
+Input:  skillrack 
+
+Output: -1
+
+SOLUTION:
+*/
+
+
+/*
+
+
+*/
