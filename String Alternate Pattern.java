2f1c1afff49deaacb282acbe6f933da1d7c1be8f
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-23 13:13:59 +0530

    Create String Alternate Pattern.java

---
+++ String Alternate Pattern.java
@@ -0,0 +1,45 @@
+/*
+The program must accept a string S as the input. 
+The program must print the desired pattern as shown in the Example Input/Output section. 
+
+Boundary Condition(s): 1 <= Length of S <= 100 
+
+Input Format: T he first line contains S. 
+
+Output Format: The lines containing the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+
+Input:  abcdefghijklmno
+
+Output: a 
+        no 
+        bcd 
+        jklm
+        efghi
+        
+Example Input/Output 2:
+
+Input:  skillrack 
+
+Output: s 
+        ck 
+        kil 
+        lra 
+        
+Example Input/Output 3: 
+
+Input:  junk 
+
+Output: j
+        nk 
+        u
+
+SOLUTION:
+*/
+
+
+/*
+
+
+*/
