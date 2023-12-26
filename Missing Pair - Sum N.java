7d65d42df3d652c3f0add4f692174537fe085bf9
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-12-26 17:48:52 +0530

    Update Missing Pair - Sum N.java

--- Missing Pair - Sum N.java
+++ Missing Pair - Sum N.java
@@ -37,9 +37,98 @@ Output: (1,5)
 SOLUTION:
 */
 
+import java.util.*;
+public class Hello 
+{
+    public static void main(String[] args) 
+    {
+		    Scanner sc = new Scanner(System.in);
+		    String t[] = new String[2];
+		    int total = 0, i = 0;
+		    Set<Integer> set = new HashSet<>();
+		    
+        while(sc.hasNext()) 
+        {
+		        t = sc.next().replaceAll("[()]", "").split(",");
+		        if((i++) == 0) total += Integer.parseInt(t[0]) + Integer.parseInt(t[1]);
+		        set.add(Integer.parseInt(t[0]));
+		    }
+      
+		    for(i = 1; i <= total / 2; i++) 
+        {
+		        if(!set.contains(i)) 
+            {
+		            System.out.print("(" + i + "," + (total - i) + ") ");
+		        }
+		    }
+	  }
+}
 
 /*
+PY3:
+l = input().split()
+p = []
+k = []
+for x in l:
+    t = tuple(map(int, x[1:-1].split(',')))
+    p.append(t)
+    k.append(t[0])
+s = sum(p[0])
+for i in range(1, s//2 + 1):
+    if i not in k:
+        t = '(' + str(i) + ','+str(s - i) + ')'
+        print(t)
+        exit(0)
 
+CPP:
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
+int main(int argc, char** argv)
+{
 
+    map<int,int>mp;
+
+    char ch;
+    int a, b, mi = INT_MAX, mx = INT_MIN, sum = 0;
+    
+    while(cin>>ch>>a>>ch>>b>>ch)
+    {
+       mp[a]++;
+       mp[b]++;
+       sum = a + b;
+    }
+    
+    for(int i = 1; ;i++)
+    {
+        if(mp[i] == 0)
+        {
+            cout<<"("<<i<<", "<<sum - i<<")";
+            break;
+        }
+    }
+}
+
+C:
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x, y, ar[1001] = {0}, s = 0;
+    while(scanf("(%d,%d) ", &x, &y) > 0)
+    {
+        s = x + y;
+        ar[x]++;
+    }
+    for(int i = 1; i <= 1001; i++)
+    {
+        if(ar[i] == 0)
+        {
+           printf("(%d, %d)", i, abs(i - s));
+           return 0;
+        }
+    }
+}
 */
